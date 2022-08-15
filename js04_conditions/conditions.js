ea4178a1ddb635d8d5fb6079620cb61fa86e37d2
Ali <alifeyza4017@gmail.com>
2022-08-15 22:00:54 +0300

    added

--- js04_conditions/conditions.js
+++ js04_conditions/conditions.js
@@ -6,12 +6,25 @@
 
 console.log("****** CONDITIONS *******");
 
-const sayi = +prompt("Bir sayi giriniz:");
-if (sayi > 0) {
-    console.log(`${sayi} pozitifdir.`);
-} else if (sayi == 0) {
-    console.log(`${asyi} 0'a esittir.`);
-} else {
-    console.log(`${sayi} negatiftir.`)
-}
-console.log(sayi, typeof sayi);
+// const sayi = +prompt("Bir sayi giriniz:");
+// if (sayi > 0) {
+//     console.log(`${sayi} pozitifdir.`);
+// } else if (sayi == 0) {
+//     console.log(`${asyi} 0'a esittir.`);
+// } else {
+//     console.log(`${sayi} negatiftir.`)
+// }
+// console.log(sayi, typeof sayi);
+
+const n1 = prompt("Sayi1:")
+const n2 = prompt("Sayi2:")
+const n3 = prompt("Sayi3:")
+
+if(n1 >= n2 && n1 >= n3) {
+    console.log(`${n1} en buyuk sayidir.`);
+} else if (n2 >= n1 && n2 >= n3) {
+    console.log(`${n2} en buyuk sayidir.`);
+} 
+ else if (n3 >= n1 && n3 >= n3) {
+    console.log(`${n3} en buyuk sayidir.`);
+} 
