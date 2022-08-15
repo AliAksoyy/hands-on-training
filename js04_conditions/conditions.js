28f9149fec2f8ea600e591562e24e4736e864f01
Ali <alifeyza4017@gmail.com>
2022-08-15 22:56:36 +0300

    added

--- js04_conditions/conditions.js
+++ js04_conditions/conditions.js
@@ -16,15 +16,27 @@ console.log("****** CONDITIONS *******");
 // }
 // console.log(sayi, typeof sayi);
 
-const n1 = prompt("Sayi1:")
-const n2 = prompt("Sayi2:")
-const n3 = prompt("Sayi3:")
+const n1 = +prompt("Sayi1:")
+const n2 = +prompt("Sayi2:")
+const n3 = +prompt("Sayi3:")
 
-if(n1 >= n2 && n1 >= n3) {
-    console.log(`${n1} en buyuk sayidir.`);
-} else if (n2 >= n1 && n2 >= n3) {
-    console.log(`${n2} en buyuk sayidir.`);
-} 
- else if (n3 >= n1 && n3 >= n3) {
-    console.log(`${n3} en buyuk sayidir.`);
-} 
+// *1.Yöntem
+
+// if(n1 >= n2 && n1 >= n3) {
+//     console.log(`${n1} en buyuk sayidir.`);
+// } else if (n2 >= n1 && n2 >= n3) {
+//     console.log(`${n2} en buyuk sayidir.`);
+// } 
+//  else if (n3 >= n1 && n3 >= n3) {
+//     console.log(`${n3} en buyuk sayidir.`);
+// } 
+
+// *2. Yöntem
+let enBuyuk = n1;
+if (n2 >= enBuyuk) {
+    enBuyuk = n2;
+}
+if (n3 >= enBuyuk) {
+    enBuyuk = n3;
+}
+alert(`${enBuyuk} en buyuktur`);
