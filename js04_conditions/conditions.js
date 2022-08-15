647c5e3456591fdee71c7634c4891c673437c460
Ali <alifeyza4017@gmail.com>
2022-08-15 23:10:00 +0300

    added

--- js04_conditions/conditions.js
+++ js04_conditions/conditions.js
@@ -16,11 +16,11 @@ console.log("****** CONDITIONS *******");
 // }
 // console.log(sayi, typeof sayi);
 
-const n1 = +prompt("Sayi1:")
-const n2 = +prompt("Sayi2:")
-const n3 = +prompt("Sayi3:")
+// const n1 = +prompt("Sayi1:")
+// const n2 = +prompt("Sayi2:")
+// const n3 = +prompt("Sayi3:")
 
-// *1.Yöntem
+// // *1.Yöntem
 
 // if(n1 >= n2 && n1 >= n3) {
 //     console.log(`${n1} en buyuk sayidir.`);
@@ -31,12 +31,27 @@ const n3 = +prompt("Sayi3:")
 //     console.log(`${n3} en buyuk sayidir.`);
 // } 
 
-// *2. Yöntem
-let enBuyuk = n1;
-if (n2 >= enBuyuk) {
-    enBuyuk = n2;
-}
-if (n3 >= enBuyuk) {
-    enBuyuk = n3;
-}
-alert(`${enBuyuk} en buyuktur`);
+// // *2. Yöntem
+// let enBuyuk = n1;
+// if (n2 >= enBuyuk) {
+//     enBuyuk = n2;
+// }
+// if (n3 >= enBuyuk) {
+//     enBuyuk = n3;
+// }
+// alert(`${enBuyuk} en buyuktur`);
+
+
+// const not = +prompt("Notunuzu Giriniz");
+// let sonuc = not >= 50 ? "Gecti" : "Kaldi";
+// console.log(sonuc);
+
+const yas = 17;
+const cinsiyet = "erkek";
+const saglikli = true;
+
+yas >= 18 && cinsiyet === "erkek" && saglikli
+? console.log("askerlik yapmali")
+: console.log("Askerlik yapmasına gerek yok");
+
+
