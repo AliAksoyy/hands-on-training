3dec75055a47d96bb76dc2e4fa0764896d73b3ad
Ali <alifeyza4017@gmail.com>
2022-08-15 21:46:54 +0300

    added

---
+++ js04_conditions/conditions.js
@@ -0,0 +1,17 @@
+// * ===============================================
+// *                  KARAR YAPILARI
+// * ===============================================
+
+//**************** IF-ELSE *******************
+
+console.log("****** CONDITIONS *******");
+
+const sayi = +prompt("Bir sayi giriniz:");
+if (sayi > 0) {
+    console.log(`${sayi} pozitifdir.`);
+} else if (sayi == 0) {
+    console.log(`${asyi} 0'a esittir.`);
+} else {
+    console.log(`${sayi} negatiftir.`)
+}
+console.log(sayi, typeof sayi);
