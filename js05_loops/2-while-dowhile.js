5a9556210d00e05ce156a75bcd1d5941ebe31abb
Ali <alifeyza4017@gmail.com>
2022-08-17 22:27:05 +0300

    added

--- js05_loops/2-while-dowhile.js
+++ js05_loops/2-while-dowhile.js
@@ -8,10 +8,24 @@ console.log("****** WHILE *******");
 //? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
 //? not girmeye zorlayan kodu while dongusu ile yaziniz.
 
-const not = prompt("Lütfen 0-100 arasında bir not giriniz");
+// const not = prompt("Lütfen 0-100 arasında bir not giriniz");
 
-while (not < 0 || not > 100) {
-    console.log("Not 0-100 arasinda olmalidir") ;
-    not = prompt("Lütfen 0-100 arasında bir not giriniz")   
+// while (not < 0 || not > 100) {
+//     console.log("Not 0-100 arasinda olmalidir") ;
+//     not = prompt("Lütfen 0-100 arasında bir not giriniz")   
+// }
+// console.log("girdiginiz not:", not)
+
+//? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
+//? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
+//? not girmeye zorlayan kodu do-while dongusu ile yaziniz
+
+let not1;
+do {
+    not1 = +prompt("Lütfen 0-100 arasında bir not giriniz");
+    if(not1 < 0 || not1 > 100) {
+        console.log("not 0-100 arasında olmalıdır")
+    }
 }
-console.log("girdiginiz not:", not)
+while (not1 < 0 || not1 > 100) 
+console.log("girdiginiz not:", not1)
