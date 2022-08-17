65b2622a84efe5df21737e030178b3445842b3db
Ali <alifeyza4017@gmail.com>
2022-08-17 21:01:12 +0300

    added

--- js05_loops/1-for.js
+++ js05_loops/1-for.js
@@ -2,11 +2,28 @@
 // *            DONGULER(LOOPS)- FOR
 // * ============================================
 
-console.log("****** FOR *******");
+// ! 1 inci proje
 
-const n = +prompt("bir sayi giriniz")
+// console.log("****** FOR *******");
 
-let toplam = 0;
-for ( let i=1; i<=n; i++) {
-    toplam +=i;
+// const n = +prompt("bir sayi giriniz")
+
+// let toplam = 0;
+// for ( let i=1; i<=n; i++) {
+//     toplam +=i;
+//     console.log(toplam, i)
+// }
+// console.log("sonuc:", toplam);
+
+
+// ? 2 nci proje 0-100 arasında 10 ader rastgele tam sayı üreten kodu yazın
+
+//* Math.floor() =>surekli asagiya yuvarlar
+//* Math.ceil() =>sürekli yukariya yuvarlar
+//* Math.round() => degerine gore yuvarlar.
+//* Math.trunc() => kesirli sayinin tam 
+
+for(let i = 1; i <= 10; i++) {
+    const rastgele = Math.round(Math.random() * 100);
+    console.log(rastgele)
 }
