a59fcb5f95ed051e660addb19c09e9f189c864d4
Ali <alifeyza4017@gmail.com>
2022-08-19 11:33:04 +0300

    added

--- js05_loops/2-while-dowhile.js
+++ js05_loops/2-while-dowhile.js
@@ -222,6 +222,11 @@ if(favourite === 23) {
     console.log("Cool 23 is an amazing number")
 } else if (favourite === 7) {
     console.log("7 is also a cool number")
-} else {
-    console.log("Number is not 23 or 7")
+} else if (favourite === 9) {
+console.log("9 is also a cool number");
 }
+ else {
+    console.log("Number is not 23 or 7")
+   
+}
+ if (favourite !== 23) console.log("Why not 23?");
