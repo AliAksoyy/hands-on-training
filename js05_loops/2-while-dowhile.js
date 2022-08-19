ec1c2a86e6e0f2f113a8817d679d81bba69d5d1c
Ali <alifeyza4017@gmail.com>
2022-08-19 11:44:21 +0300

    added

--- js05_loops/2-while-dowhile.js
+++ js05_loops/2-while-dowhile.js
@@ -211,22 +211,30 @@ console.log("****** WHILE *******");
 
 // **equality operators
 
-let age = 18;
-if(age === 18) console.log("You just became an adult (strict)")
-if(age == 18) console.log("You just became an adult (loose)")
+// let age = 18;
+// if(age === 18) console.log("You just became an adult (strict)")
+// if(age == 18) console.log("You just became an adult (loose)")
 
-const favourite = Number(prompt("What's your favourite number?"))
-console.log(favourite, typeof favourite);
+// const favourite = Number(prompt("What's your favourite number?"))
+// console.log(favourite, typeof favourite);
 
-if(favourite === 23) {
-    console.log("Cool 23 is an amazing number")
-} else if (favourite === 7) {
-    console.log("7 is also a cool number")
-} else if (favourite === 9) {
-console.log("9 is also a cool number");
-}
- else {
-    console.log("Number is not 23 or 7")
+// if(favourite === 23) {
+//     console.log("Cool 23 is an amazing number")
+// } else if (favourite === 7) {
+//     console.log("7 is also a cool number")
+// } else if (favourite === 9) {
+// console.log("9 is also a cool number");
+// }
+//  else {
+//     console.log("Number is not 23 or 7")
    
-}
- if (favourite !== 23) console.log("Why not 23?");
+// }
+//  if (favourite !== 23) console.log("Why not 23?");
+
+// ****Boolean Logic,Logical operations
+
+const a =  true;
+const b = true;
+console.log(a && b)
+console.log(a && !b)
+console.log(!a || b)
