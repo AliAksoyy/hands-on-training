f1d1085132e1deb53788149f4b65b2a96d1fda80
Ali <alifeyza4017@gmail.com>
2022-08-19 09:48:36 +0300

    added

--- js05_loops/2-while-dowhile.js
+++ js05_loops/2-while-dowhile.js
@@ -170,3 +170,8 @@ console.log("****** WHILE *******");
 const inputYear = "1991";
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
+
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+console.log(String(23), 23)
