80a6b545bd65cfc430ae780f537851d893eb38f5
Ali <alifeyza4017@gmail.com>
2022-08-20 13:28:00 +0300

    added

--- js06_function/1-functionDeclaration.js
+++ js06_function/1-functionDeclaration.js
@@ -50,15 +50,17 @@ console.log("****** FUNC DECLARATION *********");
 
 
 // ***
-function tekCift(sayi) {
-  return sayi % 2 ? `${sayi} tektir` : `${sayi} ciftdir`;
+// function tekCift(sayi) {
+//   return sayi % 2 ? `${sayi} tektir` : `${sayi} ciftdir`;
         
-}
-console.log(tekCift(5));
-console.log(tekCift(2));
+// }
+// console.log(tekCift(5));
+// console.log(tekCift(2));
 
 
-// ***** örnek 4
+// // ***** örnek 4
 
-const sayi = +prompt("bir sayi giriniz");
-console.log(tekCift(sayi));
+// const sayi = +prompt("bir sayi giriniz");
+// console.log(tekCift(sayi));
+
+// ***
