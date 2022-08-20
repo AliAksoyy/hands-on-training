18ed8ea294c61f4b358b244f3e417abfaa4a1225
Ali <alifeyza4017@gmail.com>
2022-08-20 12:17:40 +0300

    added

--- js06_function/1-functionDeclaration.js
+++ js06_function/1-functionDeclaration.js
@@ -10,3 +10,13 @@ console.log("****** FUNC DECLARATION *********");
 
 //* ORNEK:
 //************************************************/
+
+
+
+yazdir();
+// !fonksiyonun tanımlanması (declaration)
+function yazdir () {
+    console.log("merhaba");
+}
+yazdir ()    //!!invoke called
+yazdir ();
