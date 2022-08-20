f408e91ea7671adbe6bf4ca9e5a0f8da204acbb0
Ali <alifeyza4017@gmail.com>
2022-08-20 13:24:49 +0300

    added

--- js06_function/1-functionDeclaration.js
+++ js06_function/1-functionDeclaration.js
@@ -13,37 +13,52 @@ console.log("****** FUNC DECLARATION *********");
 
 
 
-yazdir();
-// !fonksiyonun tanımlanması (declaration)
-function yazdir () {
-    console.log("merhaba");
+// yazdir();
+// // !fonksiyonun tanımlanması (declaration)
+// function yazdir () {
+//     console.log("merhaba");
+// }
+// yazdir ()    //!!invoke called
+// yazdir ();
+
+
+// // *** örenek -2
+// //! Bir parametreyi cagirma sirasinda kullanmaz isek onun
+// //! yerine default parametre atayabiliriz. Ornekteki lastName
+// //! parametresi icin default değer olarak '' atanmistir.
+
+// function selamlama(ad,soyAd = "") {
+//     console.log(`Merhaba ${ad} ${soyAd}`)
+// }
+// selamlama("Can", "Yılmaz");
+// selamlama("Canan", "Ozturk");
+// selamlama("Ayse", );
+
+// // *** örenek-3
+
+
+// function yasHesapla(isim,dogumTarihi) {
+//     // const sonuc = `${isim} in yasi ${2022 - dogumTarihi} dir`;
+//     // const sonuc = `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir`;
+//     // return sonuc;
+//     return `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir`;
+
+// }
+// const mesaj1 = yasHesapla("Elif Can", 1990);
+// console.log(mesaj1);
+// console.log(yasHesapla("veli Canan",1980))
+
+
+// ***
+function tekCift(sayi) {
+  return sayi % 2 ? `${sayi} tektir` : `${sayi} ciftdir`;
+        
 }
-yazdir ()    //!!invoke called
-yazdir ();
+console.log(tekCift(5));
+console.log(tekCift(2));
 
 
-// *** örenek -2
-//! Bir parametreyi cagirma sirasinda kullanmaz isek onun
-//! yerine default parametre atayabiliriz. Ornekteki lastName
-//! parametresi icin default değer olarak '' atanmistir.
+// ***** örnek 4
 
-function selamlama(ad,soyAd = "") {
-    console.log(`Merhaba ${ad} ${soyAd}`)
-}
-selamlama("Can", "Yılmaz");
-selamlama("Canan", "Ozturk");
-selamlama("Ayse", );
-
-// *** örenek-3
-
-
-function yasHesapla(isim,dogumTarihi) {
-    // const sonuc = `${isim} in yasi ${2022 - dogumTarihi} dir`;
-    // const sonuc = `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir`;
-    // return sonuc;
-    return `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir`;
-
-}
-const mesaj1 = yasHesapla("Elif Can", 1990);
-console.log(mesaj1);
-console.log(yasHesapla("veli Canan",1980))
+const sayi = +prompt("bir sayi giriniz");
+console.log(tekCift(sayi));
