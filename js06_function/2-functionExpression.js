8c745298cc478b177492129add56c9d981634b2f
Ali <alifeyza4017@gmail.com>
2022-08-20 13:32:57 +0300

    added

--- js06_function/2-functionExpression.js
+++ js06_function/2-functionExpression.js
@@ -10,3 +10,10 @@ console.log("******** 2- EXPRESSION*******");
 
 //* ORNEK:
 //***************************************************/
+
+// !! Expression ile tanımlama
+
+const tekCift1 = function (sayi) {
+    return sayi % 2 ? `${sayi} TEKTİR` : `${sayi} CIFTDIR`;
+};
+console.log(tekCift1(5))
