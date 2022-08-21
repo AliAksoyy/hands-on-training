d5b9c2f6911d877587346d8bffb92c6d529422b3
Ali <alifeyza4017@gmail.com>
2022-08-21 09:08:34 +0300

    added

--- js05_loops/new.js
+++ js05_loops/new.js
@@ -9,23 +9,45 @@
 // }
 
 // ***
-let randomStuff = [
-  "apple",
-  312,
-  555,
-  true,
-  false,
-  undefined,
-  ["appşe", "ball", "cat"],
-  2,
-  4,
-  "lastobject",
-];
+// let randomStuff = [
+//   "apple",
+//   312,
+//   555,
+//   true,
+//   false,
+//   undefined,
+//   ["appşe", "ball", "cat"],
+//   2,
+//   4,
+//   "lastobject",
+// ];
 
-let num = randomStuff.length
-while(num>=10) {
-    console.log(num)
-    console.log(randomStuff[num]);
-    num -=1
-}
-    
+// let num = randomStuff.length
+// while(num>=10) {
+//     console.log(num)
+//     console.log(randomStuff[num]);
+//     num -=1
+// }
+    // ***
+    function ali() {
+        console.log("beyda")
+    }
+    ali()
+
+    function ali1(isim) {
+        console.log(`${isim}`)
+    }
+    ali1("ali")
+
+    function ali2(a,b,c,d) {
+        console.log(a+b+c+4)
+    }
+    ali2(1,2,3,4)
+
+    function ali3Array(mango) {
+        for(let i = 0; i<mango.length; i++) {
+            console.log(mango[i])
+            
+        }
+    }
+    ali3Array(["cat","wer","redbull","ayse","are","dfdsf","ere"])
