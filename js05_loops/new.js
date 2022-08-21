263ab39c3c221ec259b839811d45bdbf2c46ff67
Ali <alifeyza4017@gmail.com>
2022-08-21 08:40:33 +0300

    added

--- js05_loops/new.js
+++ js05_loops/new.js
@@ -1,5 +1,9 @@
 let randomStuff = ["apple", 312, 555, true,false, undefined, ["appşe","ball","cat"],2,4,"lastobject"]
 let x = 0;
-while(x<randomStuff.length) {
+while(x<=randomStuff.length) {
   console.log(x)
   x++;
+  
+ 
+  console.log(randomStuff[x]);
+}
