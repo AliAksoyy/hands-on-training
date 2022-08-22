69d895dda057af97d6261fd16dd346caad79e078
Ali <alifeyza4017@gmail.com>
2022-08-22 20:55:36 +0300

    added

--- js07_function-2/1-arrowFunction.js
+++ js07_function-2/1-arrowFunction.js
@@ -11,8 +11,30 @@ console.log("**** ARROW ******");
 //* ORNEK:
 //************************************************/
 
-const r = Number(prompt("yarıcap giriniz"))
-const h = Number(prompt("yukseklik giriniz"))
+// const r = Number(prompt("yarıcap giriniz"))
+// const h = Number(prompt("yukseklik giriniz"))
 
-const silindirHacmi = (r,h) => Math.PI*r*r*h;
-console.log(`${r},${h} olan silindir hacimi = ${silindirHacmi(r,h).toFixed(2)}`)
+// const silindirHacmi = (r,h) => Math.PI*r*r*h;
+// console.log(`${r},${h} olan silindir hacimi = ${silindirHacmi(r,h).toFixed(2)}`)
+
+
+const tarih = Number(prompt("Dogum Tarihini giriniz:"));
+
+//! Arrow Func yontemi ile tanimlama
+const yasHesapla = (tarih) => {
+  const yas = new Date().getFullYear() - tarih;
+  return yas;
+};
+
+// console.log("YASINIZ:" + yasHesapla2(tarih));
+//!Funct expression ve arrow func yontemlerinde
+//! Once fonks tanimlanmalidir sonra cagrilmalidir.
+//! Aksi takdirde hata alrsiniz.
+
+//! Func Expression yontemi ile tanimlama
+const yasHesapla2 = function (tarih) {
+  const yas = new Date().getFullYear() - tarih;
+  return yas;
+};
+
+console.log("YASINIZ:" + yasHesapla(tarih));
