c9432a8e1763ff1a4d780e1d6a4781fe35a8a59b
Ali <alifeyza4017@gmail.com>
2022-08-22 06:47:30 +0300

    addedd

--- js05_loops/jonas.js
+++ js05_loops/jonas.js
@@ -1,4 +1,24 @@
 'use strict';
 
-const interface = "audio"
-console.log(interface)
+// const interface = "audio"
+// console.log(interface)
+// **** function
+
+function logger () {
+    console.log("My name is Ali")
+}
+
+logger()
+logger()
+logger()
+
+function fruit(apples,oranges) {
+console.log(apples,oranges)
+const juice = `Juice with ${apples} apples and ${oranges} oranges`
+return juice
+
+}
+fruit(4,5)
+console.log(fruit(4,5))
+const applejuice = fruit(3,0)
+console.log(applejuice)
