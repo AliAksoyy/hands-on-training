697c6ff54b1b3727bd7ea1947556d4e7cef1c5e9
Ali <alifeyza4017@gmail.com>
2022-08-22 22:14:31 +0300

    added

--- js07_function-2/1-arrowFunction.js
+++ js07_function-2/1-arrowFunction.js
@@ -18,23 +18,64 @@ console.log("**** ARROW ******");
 // console.log(`${r},${h} olan silindir hacimi = ${silindirHacmi(r,h).toFixed(2)}`)
 
 
-const tarih = Number(prompt("Dogum Tarihini giriniz:"));
+// const tarih = Number(prompt("Dogum Tarihini giriniz:"));
 
+// //! Arrow Func yontemi ile tanimlama
+// const yasHesapla = (tarih) => {
+//   const yas = new Date().getFullYear() - tarih;
+//   return yas;
+// };
+
+// // console.log("YASINIZ:" + yasHesapla2(tarih));
+// //!Funct expression ve arrow func yontemlerinde
+// //! Once fonks tanimlanmalidir sonra cagrilmalidir.
+// //! Aksi takdirde hata alrsiniz.
+
+// //! Func Expression yontemi ile tanimlama
+// const yasHesapla2 = function (tarih) {
+//   const yas = new Date().getFullYear() - tarih;
+//   return yas;
+// };
+
+// console.log("YASINIZ:" + yasHesapla(tarih));
+
+// **** 
 //! Arrow Func yontemi ile tanimlama
-const yasHesapla = (tarih) => {
-  const yas = new Date().getFullYear() - tarih;
-  return yas;
+// const yasHesapla = (tarih) => {
+//   const yas = new Date().getFullYear() - tarih;
+//   return yas;
+// };
+
+// //* alternatif
+// const yasHesaplaKisa = (tarih) => new Date().getFullYear() - tarih;
+
+// // console.log("YASINIZ:" + yasHesapla(tarih));
+// console.log("YASINIZ:" + yasHesaplaKisa(tarih));
+// ******** 
+//* ORNEK: Girilen n. terime kadar Fibonacci sayilarinin toplamini
+//* hesaplayarak yazdiran fonksiyonu dongu ile kodlayiniz.
+//? FIBONACCI terimleri:   1, 1, 2, 3, 5, 8, 13, 21, ...
+let stringResult = "1,1";
+// f(n) = f(n-1) + f(n-2)
+const fibo = (n) => {
+  let fib1 = 1;
+  let fib2 = 1;
+  let toplam = 0;
+
+  for (let i = 2; i < n; i++) {
+    toplam = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = toplam;
+    stringResult += "," + fib2;
+  }
+
+  return fib2;
 };
 
-// console.log("YASINIZ:" + yasHesapla2(tarih));
-//!Funct expression ve arrow func yontemlerinde
-//! Once fonks tanimlanmalidir sonra cagrilmalidir.
-//! Aksi takdirde hata alrsiniz.
-
-//! Func Expression yontemi ile tanimlama
-const yasHesapla2 = function (tarih) {
-  const yas = new Date().getFullYear() - tarih;
-  return yas;
-};
-
-console.log("YASINIZ:" + yasHesapla(tarih));
+const n = +prompt("n terimini giriniz:");
+if (n <= 0) {
+  console.log("Lutfen 0 dan buyuk bir sayi giriniz.");
+} else {
+  console.log(`FIBO(${n})= ${fibo(n)}`);
+  console.log(stringResult);
+}
