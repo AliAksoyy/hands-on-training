8bfc6966c6325060bf039a1e7397ddd3b31c902d
Ali <alifeyza4017@gmail.com>
2022-08-24 21:30:11 +0300

    added

--- js08_strings/stringMethods.js
+++ js08_strings/stringMethods.js
@@ -70,4 +70,13 @@ console.log(esitMi("Hello" ,"Hell"))
 
 // ****localeCompare()
 
+const a = "réservé"; // with accents, lowercase
+const b = "RESERVE"; // no accents, uppercase
 
+console.log(a.localeCompare(b));
+// expected output: 1
+console.log(a.localeCompare(b, "en", { sensitivity: "base" }));
+
+//? 'BASE: a ≠ b, a = á, a = A'
+//? 'ACCENT: a ≠ b, a ≠ á, a = A'
+//? 'CASE: a ≠ b, a = á, a ≠ A'
