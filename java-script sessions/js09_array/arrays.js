cb53b92e5112df68cb3d084b1722a6dd680746fa
Ali <alifeyza4017@gmail.com>
2022-08-29 20:53:21 +0300

    added

--- java-script sessions/js09_array/arrays.js
+++ java-script sessions/js09_array/arrays.js
@@ -7,16 +7,48 @@ console.log("***** ARRAYS ******");
 //* Dizi Tanimlama
 //* ---------------------------------------------------------
 
-const isimler =["ahmet", "ali", "beyda", "feyza"]
-
+// ! 1.Yöntem (Array Literal) - Tercih edilen yöntem
+const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
 console.log(isimler);
-console.log(typeof isimler)
-console.log(isimler.length)
+console.log(typeof isimler);
+console.log(isimler.length);
 
-//! 2 yöntem
+const bosDizi = [];
+console.log(bosDizi);
 
-const diller = new Array("C+", "javascript", "assembly", "go", "Ruby")
-console.log(diller)
+// ! 2.Yöntem (Object Constructor)
+const diller = new Array("C++", "javascript", "Assembly", "Go", "Ruby");
+console.log(diller);
 
-const a = new Array("ali")
-console.log(a)
+const numbers = new Array(3, 2, 1);
+console.log(numbers);
+
+//! 10 elemanlik bos bir Array oluşturdu
+const numbers1 = new Array(10);
+console.log(numbers1);
+
+// ! 3.Yöntem (Array.of())
+const veriler = Array.of(1, 2, 3);
+console.log(veriler);
+
+const veri = Array.of("Deneme");
+console.log(veri);
+
+//* Diziden Veri Okuma-Yazma (indisleme)
+//* ---------------------------------------------------------
+//!  Diziden veri okuma
+console.log(diller[1]);
+const go = diller[3];
+console.log(go);
+
+console.log(isimler[isimler.length - 1]);
+//? at()
+console.log(isimler.at(-1));
+
+//!  Diziye veri yazma
+// const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
+isimler[1] = "Saffet";
+console.log(isimler);
+
+//!Uncaught TypeError: Assignment to constant variable.
+isimler = ["Ali", "Veli"];
