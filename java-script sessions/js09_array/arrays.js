6b6f222d23b44a40143962cc4454857e65b89cf6
Ali <alifeyza4017@gmail.com>
2022-08-29 20:39:38 +0300

    added

--- java-script sessions/js09_array/arrays.js
+++ java-script sessions/js09_array/arrays.js
@@ -12,3 +12,11 @@ const isimler =["ahmet", "ali", "beyda", "feyza"]
 console.log(isimler);
 console.log(typeof isimler)
 console.log(isimler.length)
+
+//! 2 yöntem
+
+const diller = new Array("C+", "javascript", "assembly", "go", "Ruby")
+console.log(diller)
+
+const a = new Array("ali")
+console.log(a)
