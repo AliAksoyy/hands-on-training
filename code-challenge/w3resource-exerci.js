858fa9e75c6e45d9df9e2c0617627a48aa7aa5f9
Ali <alifeyza4017@gmail.com>
2022-08-31 06:56:03 +0300

    added

--- code-challenge/w3resource-exerci.js
+++ code-challenge/w3resource-exerci.js
@@ -606,9 +606,7 @@
 // };
 // console.log(mostFreq([3, "b", 2, "a", 2, 3, 3, "a", 2, 4, 9, 2]));
 
-// for (let j = 0; j < newBuyuk.length; j++) {
-//   newBuyuk2 = newBuyuk[i][0].toLowerCase();
-// }
+
 
 // *******************9********************
 // 9. Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'. Go to the editor
