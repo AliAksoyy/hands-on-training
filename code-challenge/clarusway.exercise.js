9e579ae2ca88eaae344567b9d84d51d3156a2879
Ali <alifeyza4017@gmail.com>
2022-09-01 09:53:39 +0300

    added

--- code-challenge/clarusway.exercise.js
+++ code-challenge/clarusway.exercise.js
@@ -325,22 +325,38 @@
 
 
 
-//?-------------- ÖRNEK -------------------
-const dizi = [-5, 15, 22, -4, 45, 78];
+// //?-------------- ÖRNEK -------------------
+// const dizi = [-5, 15, 22, -4, 45, 78];
 
-const negatifler = []
-const pozitifler = []
+// const negatifler = []
+// const pozitifler = []
 
-const dizilereAyır = (n) => {
-    for(let i = 0; i<n.length; i++) {
-        if(n[i] <0) {
-            negatifler.push(n[i])
-        } else {
-            pozitifler.push(n[i])
-        }
-    }
-    return dizi
-}
-console.log( dizilereAyır(dizi))
-console.log(negatifler)
-console.log(pozitifler)
+// const dizilereAyır = (n) => {
+//     for(let i = 0; i<n.length; i++) {
+//         if(n[i] <0) {
+//             negatifler.push(n[i])
+//         } else {
+//             pozitifler.push(n[i])
+//         }
+//     }
+//     return dizi
+// }
+// console.log( dizilereAyır(dizi))
+// console.log(negatifler)
+// console.log(pozitifler)
+
+
+
+// //?-------------- ÖRNEK -------------------
+// //? Dizideki notlarin ortalamasini hesaplayiniz.
+// const notlar = [55, 77, 23, 89, 100];
+
+// let toplam = 0
+
+
+
+// for(let i= 0; i<notlar.length; i++) {
+
+//     toplam += notlar[i]
+// }
+// console.log("ORTALAMA :" + " " +toplam / notlar.length)
