bdf2bbd8a391032e8acba0f367474819cdf6ed79
Ali <alifeyza4017@gmail.com>
2022-09-01 11:10:00 +0300

    aadded

--- code-challenge/clarusway.exercise.js
+++ code-challenge/clarusway.exercise.js
@@ -480,3 +480,15 @@
 // //?For Capitilize
 // const c =products.map((p,i,arr) => arr[i] =p[0].toUpperCase() + p.slice(1))
 // console.log(c)
+
+
+const salaries = [5500, 8000, 6500, 9000, 10000, 15000, 25000];
+
+//?-------------- ÖRNEK -------------------
+//? Maasi 10000'den buyuk olanlari ayri bir diziye saklayalim
+
+const bigThan = salaries.filter((s) => s > 10000);
+console.log(bigThan, salaries);
+
+const range = salaries.filter((s) => s >= 6000 && s <= 10000);
+console.log(range);
