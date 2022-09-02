454ed1434de9c494bd092707a577bb5236cf1c6f
Ali <alifeyza4017@gmail.com>
2022-09-02 12:25:32 +0300

    added

--- code-challenge/clarusway.exercise.js
+++ code-challenge/clarusway.exercise.js
@@ -774,3 +774,17 @@
 //   console.log(`${sayi} ${sonuc}`);
 // }
 
+
+//?ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
+//? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
+//? not girmeye zorlayan kodu while dongusu ile yaziniz.
+
+
+
+
+// const not = +prompt("sayı gir")
+//  while(not<0 || not>100) {
+//     alert("aralık yanlış")
+//     not = +prompt("yeniden gir")
+//  }
+//  console.log("girdiğibiz not :",not)
