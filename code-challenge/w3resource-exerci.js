cd8b8947a7cbd627b0ab01b6761e8b54d3001164
Ali <alifeyza4017@gmail.com>
2022-09-02 17:08:04 +0300

    added

--- code-challenge/w3resource-exerci.js
+++ code-challenge/w3resource-exerci.js
@@ -577,7 +577,37 @@
 
 // 20. Write a JavaScript function that generates a string id (specified length) of random characters. Go to the editor
 // Sample character list : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-// Click me to see the solution.
+
+// function id(len) {
+//     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//     len = len || 32;
+//     let str_id = "";
+
+//     for (let i = 0; i < len; i++) {
+//         str_id += str.charAt(Math.floor(Math.random() * str.length));
+//     }
+//     return str_id;
+// }
+
+// console.log(id(7));
+// console.log(id(10));
+// console.log(id());
+
+
+function makeId(n) {
+  let lstChar =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789!@#$%^&*",
+    id = "";
+
+  for (var i = 0; i < n; i++) {
+    let rnd = Math.floor(lstChar.length * Math.random());
+    console.log(rnd)
+    id += lstChar.charAt(rnd);
+  }
+  console.log(id);
+}
+makeId(4);
+
 
 // *******************21***********
 
