4ae418c318dd065cb5a65da510015fb9d047d40c
Ali <alifeyza4017@gmail.com>
2022-09-02 12:10:49 +0300

    added

--- code-challenge/clarusway.exercise.js
+++ code-challenge/clarusway.exercise.js
@@ -744,3 +744,33 @@
 // }
 // console.log(t)
 
+//? Ornek: 0-100 Arasinda 10 adet rasgele tamsayi sayi ureten kodu
+//? for dongusu ile yaziniz..
+
+// for(let i = 0; i<10; i++) {
+
+//     const rast = Math.round(Math.random() *100)
+//     console.log(rast);
+
+// }
+
+//? ÖRNEK: Girilen bir sayinin Asal olup olmadigini yazdiran kodu
+//? for dongulerini kullanarak yaziniz.
+
+// const sayi = Number(prompt("Pozitif Bir sayi giriniz:"));
+// let asal = true;
+
+// if (sayi <= 1) {
+//   alert("Sayi 1'den buyuk olmalidir.");
+// } else {
+//   for (let i = 2; i < sayi; i++) {
+//     if (sayi % i === 0) {
+//       asal = false;
+//       break;
+//     }
+//   }
+
+//   const sonuc = asal === true ? "ASALDIR" : "ASAL DEGILDIR";
+//   console.log(`${sayi} ${sonuc}`);
+// }
+
