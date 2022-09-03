1c44aa9ea4e770361815c090a8f7b4781ba4791a
Ali <alifeyza4017@gmail.com>
2022-09-03 15:56:08 +0300

    added

--- code-challenge/clarusway.exercise.js
+++ code-challenge/clarusway.exercise.js
@@ -105,7 +105,271 @@
 //         }
 //         console.log( hesapla("MCMXCIV"))
 
-// ***********************4****************
+// ?99999999999999999999999999999999   29.agusts dersi ***********************
+// Write a JavaScript code to find the area of a triangle.
+
+// pseudocode:
+// a, b and c sides of a triangle
+// assume bottom = c
+// semiperimeter = s
+// s = (a + b + c) / 2
+// (s(s-a)(s-b)(s-c))**0.5
+
+// const areaOfTriangle = (a, b, c) => {
+//     let s = (a + b + c) / 2;
+//     return (s * (s - a) * (s - b) * (s - c)) ** 0.5;
+// };
+
+// console.log(areaOfTriangle(3, 4, 5));
+
+// Write a JavaScript program to display the difference between a given number and 20, if the number is greater than 20 display double the absolute difference.
+
+// pseudode:
+// if given number is greater than 20 or not.
+// if false 20 - given number
+// if true abs((20 - given number) * 2) or multiply * (-1)
+
+// const absoluteResult = (num) => {
+//     if (num < 20) {
+//         return 20 - num;
+//     } else {
+//         return Math.abs((20-num) * 2);
+//     }
+// };
+
+// console.log(absoluteResult(15));
+// console.log(absoluteResult(35));
+
+// Write a JavaScript program that takes two integers from a user and displays the larger.
+
+// pseudocode:
+// get two numbers
+// compare them with if first greater than second one 
+// compare them with else first equals or smaller than second one 
+
+// let number1 =  5;//prompt('pls enter a number');
+// let number2 =  6;//prompt('pls enter second number');
+
+// const largerNumOfTwo = (number1, number2) => {
+//     if (number1 > number2) {
+//         return `${number1} is greater than ${number2}`;
+//     } else if (number1 === number2) {
+//         return `${number1} equals ${number2}`;
+//     } else {
+//         return `${number2} is greater than ${number1}`;
+
+//     }
+// };
+
+// console.log(largerNumOfTwo(5, 6));
+
+
+// Write a JavaScript program that takes 3 integers from a user and displays the largest.
+
+// const largestNumOfThree = (number1, number2, number3) => {  // array ise spread ile açmak gerekir
+//     // return Math.max(...[number1, number2, number3]);
+//     let largest;
+
+
+//     if (number1 > number2 && number1 > number3) {
+//         largest = number1;
+//     }
+
+//     else if (number2 > number1 && number2 > number3) {
+//         largest = number2;
+
+//     }
+//     else {
+//         largest = number3;
+//     }
+//     return `${largest} is the largest`;
+// };
+
+// console.log(largestNumOfThree(2, 6, 9));
+
+
+// Write a JavaScript program that takes 3 integers from a user, multiplies all these numbers, and displays the sign of the product.
+// Example1: 
+// number1 = 5, number2 = -4, number3 = 2 ⇒ product is -40. Then the sign is “-”
+// Example2: 
+// number1 = -5, number2 = -4, number3 = 2 ⇒ product is 40. Then the sign is “+”
+
+// pseudocode:
+// if product is greater than zero sign is + else -
+
+// const multiplyOfThreeNum = (numee1, numee2, numee3) => {
+//     let product = numee1 * numee2 * numee3;
+//     return product > 0 ? "the sign is +" : "the sign is -";
+// };
+
+// console.log(multiplyOfThreeNum(1, -8, 75));
+
+// Write a JavaScript program to check whether a given integer is in the range 50 to 100.
+
+// pseudocode:
+// if num is greater than 50 && is smaller than 100 => num is in the range 50 to 100;
+// else num is not in the range 50 to 100;
+
+// const isInTheRange50To100 = (numberee) => {
+//     if (numberee > 50 && numberee < 100) {
+//         return `${numberee} is in the range 50 to 100`;
+//     } else {
+//         return `${numberee} is NOT in the range 50 to 100`;
+//     }
+// };
+
+// console.log(isInTheRange50To100(51));
+// console.log(isInTheRange50To100(49));
+
+// Write a basic Calculator with JavaScript. Ask the user to enter 2 numbers and an operand (+,-,*,/). Execute the desired operation and display the result.
+
+// Example:
+// number1 = 5, number2 = 6, operand = * ⇒ result is 5 * 6 = 30
+// pseudocode:
+// parameters a, b, operand
+// switch case
+
+// const calc = (a, b, operand) => {
+//     let result;
+
+//     switch (operand) {
+//         case '+':
+//             result = a + b;    
+//             break;
+//         case '-':
+//             result = a - b;
+//             break;
+//         case '/':
+//             result = a / b;    
+//             break;
+//         case '*':
+//             result = a * b;    
+//             break;
+    
+//         default:
+//             return 'invalid process'; 
+//     }
+//     return result;
+// };
+
+// console.log(calc(2, 9, '+'));
+// console.log(calc(2, 9, '-'));
+// console.log(calc(2, 9, '/'));
+// console.log(calc(2, 9, '*'));
+// console.log(calc(2, 9, 'ğ'));
+
+
+// Write a JavaScript program that asks the user to enter a day, check this day, and display whether a weekday or not.
+// Example1:
+// input = Tuesday ⇒ output: Tuesday is a weekday
+// Example2:
+// input = Saturday⇒ output: Saturday is not a weekday
+
+// const isWeekday = (day) => {
+//     let res;
+
+//     switch (day.toLowerCase()) {
+//         case "monday":
+//         case "tuesday":
+//         case "wednesday":
+//         case "thursday":
+//         case "friday":
+//             res = `${day} is a weekday`;
+
+//             break;
+//         case "saturday":
+//         case "sunday":
+//             res = `${day} is NOT a weekday`;
+
+//             break;
+
+//         default:
+//             return 'invalid process';
+//     };
+//     return res;
+// };
+
+// console.log(isWeekday('MONDAY'));
+// console.log(isWeekday('Saturday'));
+// console.log(isWeekday('osman'));
+
+
+// Write a JavaScript program that tells the user provided number is odd or even.
+
+// pseudocode: 
+// num % 2
+
+// const oddOrEven = (x) => {
+//     // return x % 2 === 0 ? 'even' : 'odd';
+//     return x % 2 ? 'odd' : 'even';
+// };
+
+// console.log(oddOrEven(2));
+// console.log(oddOrEven(3));
+// console.log(oddOrEven(-1));
+
+
+// Write a JavaScript program to find a value that is nearest to 100 from two different given integer values.
+
+// pseudocode:
+// smaller result difference from 100 is the nearest
+
+// const nearestNum = (y, z) => {
+//     let diffY = Math.abs(100 - y);
+//     let diffZ = Math.abs(100 - z);
+
+//     if (diffY > diffZ) {
+//         return `${z} is the nearest to 100`;
+//     } else if (diffY === diffZ) {
+//         return `they are equally close`;
+//     } else {
+//         return `${y} is the nearest to 100`;
+//     }
+// };
+
+// console.log(nearestNum(25, -96));
+// console.log(nearestNum(95, 105));
+// console.log(nearestNum(98, 99));
+
+
+
+
+// Write a JavaScript program to determine a year is a leap year in the Gregorian calendar. (Please search the conditions for the leap year)
+
+// pseudocode:
+
+// (0-99) only divisible by 4 --> leap year
+// (100-400) divided by 4 and  if not divisible by 100 --> leap year
+// if divisible by 100 and divisible by 400 --> leap year
+
+
+// first way:
+// const isLeapYear = (year) => {
+//     if (year % 100 === 0) {
+//         if (year % 400 === 0) {
+//             console.log(`${year} is a leap year.`);
+//         } else {
+//             console.log(`${year} is not a leap year.`);
+//         }
+//     } else {
+//         if (year % 4 === 0) {
+//             console.log(`${year} is a leap year.`);
+//         } else {
+//             console.log(`${year} is not a leap year.`);
+//         }
+//     }
+// }
+
+// isLeapYear(64);
+// isLeapYear(1900);
+// isLeapYear(2000);
+// isLeapYear(1912);
+
+// // second way:
+// function leapYear(year) {
+//     return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+
+
 //! Q-1
 //? Ask the user to input a number. Check the input of the user, if it is not a valid number
 //? then keep getting input from the user until provides a valid number.
@@ -321,6 +585,241 @@
 //? If user can't find the number in 5 guess then game is lost and display the random number.
 //? Hint: you can use this code to generate random number 0-100
 //? const winingNumber = Math.round(Math.random() * 100);
+
+// ********************************************************
+// !2222222222222222222222222222222222222222222222222222
+// ? 01 .09 ders challenge çalışması*******************
+
+// Write a simple JavaScript program to join all elements of the following array into a string.
+// Sample array : myColor = ["Red", "Green", "White", "Black"];
+
+
+// Expected Output :
+// "Red, Green, White, Black"
+
+// const arrToString = (arr)=>{
+//     // return arr.toString()
+//     return arr.join(", ")
+// }
+
+// console.log(arrToString(["Red", "Green", "White", "Black"]));
+// console.log(arrToString(["2","44","999"]));
+
+//**********************************SORU 2************************************** */
+
+
+// Write a JavaScript program that accepts a number as input and insert dashes (-) between each two even numbers. 
+// For example, if you accept 025468 the output should be 0-254-6-8.
+// let num="025468"
+// num=num.split("")
+// console.log(num);
+// const insertDash=(num)=>{
+//     console.log(num)
+//     let result=""
+//     for(let i=0;i<num.length;i++){
+//         if(num[i]%2==0 && num[i+1]%2==0){
+//             result+=num[i]+ "-"
+//         }else{
+//             result+=num[i]
+//         }
+//     }
+//     return result
+// }
+
+// console.log(insertDash("025468"));
+// // console.log(insertDash("669754462"));
+
+
+//***********************************SORU3************************************ */
+
+// Write a JavaScript program to sort the items of an array.
+// Sample array : var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// Sample Output : -4,-3,1,2,3,5,6,7,8
+
+
+// const sortArr=(arr)=>{
+//     // console.log(arr)
+//     // arr.sort((a,b)=>b-a)
+//     // console.log(arr);
+//     return arr.sort().reverse()
+// }
+
+// // console.log(sortArr([ 3, 8, 7, 6, 5, -4, 3, 2, 1 ]))
+// console.log(sortArr([ "aa","bb","bc","dd","ad" ]))
+
+//********************************SORU 4**************************************** */
+
+// Write a JavaScript program to find the most frequent item of an array.
+// Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+// Sample Output : a ( 5 times )
+
+// let arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]
+// console.log(arr1.sort());
+
+// const mostFrequent=(arr)=>{
+//     let most;
+//     let repetition=0;
+//     let count;
+//     arr=arr.sort()
+//     console.log(arr)
+//     for(let i=0;i<arr.length;i++){
+//         count=0
+//         for(let k=i;k<arr.length;k++){
+//             if(arr[i]==arr[k]){
+//                 count++
+//             }
+//         }
+//         if(count>repetition){
+//             repetition=count
+//             most=arr[i]
+//         }
+//     }
+//     return `${most} en çok tekrar eden sayıdır. ${repetition} kere tekrar etmiştir`
+// }
+
+// console.log(mostFrequent([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]))
+
+
+//-------------------------------------------------solution 2-----------------------------------
+// const mostFrequent=(arr)=>{
+//     // console.log(arr)
+//     let dict={}
+//     for(let i=0;i<arr.length;i++){
+//         if(!dict.hasOwnProperty(arr[i])){
+//             dict[arr[i]]=1
+//         }else{
+//             dict[arr[i]]++
+//         }
+//     }
+//     // console.log(dict)
+//     let result=Object.entries(dict)
+//     // console.log(result)
+//     result.sort((a,b)=>a[1]-b[1])
+//     // console.log(result[result.length-1])
+//     return `${result[result.length-1][0]} en çok tekrar eden sayı.${result[result.length-1][1]}  kere tekrar etmiştir`
+
+// }
+
+// console.log(mostFrequent([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]))
+
+
+//----------------------------solution 3-------------------------------
+// arr2 = [3, "a", "a", 3, "a", 3, 3, "a", "a", "b"];
+// arr2.sort();
+
+// let count = 1;
+// let most = 0;
+// let mostItem = "";
+// for (i = 0; i < arr2.length; i++) {
+//   if (arr2[i] === arr2[i + 1]) {
+//     count += 1;
+//   } else {
+//     if (count > most) {
+//       most = count;
+//       mostItem = arr2[i];
+//     }
+//     count = 1;
+//   }
+// }
+// console.log(`${mostItem} : (${most} times)`);
+
+
+//***********************************SORU 5******************************************* */
+
+// Write a JavaScript program that accepts a string as input and swap the case of each character. 
+// For example, if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+
+// toLowerCase()
+// toUpperCase()
+
+// const swapCase= (str)=>{
+//     let arr=str.split(" ")
+//     console.log(arr)
+//     let result =""
+//     for(let item of arr){
+       
+//         for(let k=0;k<item.length;k++){
+//             if(item[k]==item[k].toLowerCase()){
+//                 result+=item[k].toUpperCase()
+//             }else{
+//                 result+=item[k].toLowerCase()
+//             }
+//         }
+//         result+=" "
+        
+//     }
+// // console.log(result)
+// return result
+
+// }
+
+// console.log(swapCase('The Quick Brown Fox'));
+
+//---------------------------------------------------solution 2---------------------------
+// let str1="The Quick Brown Fox"
+// const swapCase1=(str)=>{
+//     let result=""
+//     for(let i=0;i<str.length;i++){
+//         if(str[i]==str[i].toLowerCase()){
+//             result+=str[i].toUpperCase()
+//         }else{
+//             result+=str[i].toLowerCase()
+//         }
+//     }
+//     return result
+// }
+
+// console.log(swapCase1(str1))
+
+///////////////////////////solution 3
+
+
+// let str="The Quick Brown Fox"
+// const swapCase2=(str)=>{
+//     // console.log(str[3].charCodeAt())
+//     let result=""
+//     for(let i=0;i<str.length;i++){
+//         if(str[i].charCodeAt()>=65 && str[i].charCodeAt()<=90){
+//             result+=str[i].toLowerCase()
+//         }else if(str[i].charCodeAt()>=97 && str[i].charCodeAt()<=122){
+//             result+=str[i].toUpperCase()
+//         }else{
+//             result+=str[i]
+//         }
+//     }
+// return result
+// }
+
+// console.log(swapCase2(str))
+
+
+//****************************solution 4 */********************************* */
+// let str1="The Quick Brown Fox"
+// const swapCase=(str)=>{
+//     let result=""
+//     for(let i=0;i<str.length;i++){
+//         if(str[i].match(/[A-Z]/)){
+//             result+=str[i].toLowerCase()
+//         }else if(str[i].match(/[a-z]/)){
+//             result+=str[i].toUpperCase()
+//                     }else{
+//                         result+=str[i]
+//                     }
+//     }
+// return result
+// }
+
+// console.log(swapCase(str1))
+
+
+//-----------------------------------------solution 5----------------------------
+// const swapCase=(str)=>{
+//     let arr = str.split('')
+//     console.log(arr);
+//     return arr.map(x => x === x.toLowerCase() ? x = x.toUpperCase() : x = x.toLowerCase()).join('')
+// }
+// console.log(swapCase('The Quick Brown Fox'))
+
 
 
 // !111111111111111111111 DERS 10 ARRAY2-1111111111111111
