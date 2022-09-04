02540e140a36c962c0fc25b56d7c4e8f8bde49b5
Ali <alifeyza4017@gmail.com>
2022-09-04 14:14:19 +0300

    added

--- code-challenge/w3resource-exerci.js
+++ code-challenge/w3resource-exerci.js
@@ -1030,6 +1030,25 @@
 //   }
 // }
 
+// 2.çözüm
+
+let a = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+];
+
+
+a.forEach((item, index) => {
+  console.log(`row ${index}`);
+  for (let value of item) {
+    console.log(value);
+  }
+});
+
+
 
 
 
