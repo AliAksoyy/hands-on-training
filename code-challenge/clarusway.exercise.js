54c80333416177c89826f7787062fd051601f348
Ali <alifeyza4017@gmail.com>
2022-09-05 19:12:17 +0300

    added

--- code-challenge/clarusway.exercise.js
+++ code-challenge/clarusway.exercise.js
@@ -1432,3 +1432,155 @@
 
 // console.log(`There are at least 2 numbers having same first digit: ${result}`);
 
+// **********************************************05.09  ders challenge***********************************
+
+//? 1) - verilen sayının basamak değerlerini birbiri ile çarpacağız. kaç defada tek haneli basamağa ulaşıyor?
+//? 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+//? 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+//? 4 --> 0 (because 4 is already a one-digit number)
+
+// 1çözüm
+
+// let tekhane = (sayi) => {
+//   sayi = "" + sayi;
+//   sayi = sayi.split("");
+//   let toplam = 1;
+//   let counter = 0;
+//   while (sayi.length > 1) {
+//     for (let i = 0; i < sayi.length; i++) {
+//       toplam *= sayi[i];
+//     }
+//     sayi = ("" + toplam).split("");
+//     toplam = 1;
+//     counter++;
+//   }
+//   return counter;
+// };
+// console.log(tekhane(999));
+
+//? 2) - verilen bir sayının binary sistemdeki karşılığı olan sayının içerisinde kaç adet 1 rakamı var.
+
+//? input                   output
+//? 15     =>   (1111)  =>    4 
+//? 14     =>   (1110)  =>    3
+
+
+// 1.çözüm
+
+// let binary = (sayi) => {
+
+//     sayi = sayi.toString(2)
+//     console.log(sayi)
+//     sayi = ("" + sayi).split("")
+//     let counter =0
+//     for(let item of sayi){
+//         console.log("item :", item)
+//         if(item == "1") {
+//             counter++
+//         }
+//     }
+//     return counter
+// }
+
+// console.log(binary(20))
+
+// 2.çözüm
+
+// countBits = (n) => n.toString(2).split("0").join("").length
+// countBits(20)
+
+// 3çözüm
+
+// function persistence(num) {
+//   var times = 0;
+
+//   num = num.toString();
+
+//   while (num.length > 1) {
+//     times++;
+//     num = num
+//       .split("")
+//       .map(Number)
+//       .reduce((a, b) => a * b)
+//       .toString();
+//   }
+
+//   return times;
+// }
+
+
+// ? 3)) aradaki farkı bul
+
+
+// function findMissing(normal, missing) {
+//   sumNormal = (normal.length * (normal.length + 1)) / 2;
+//   const sumWithInitial = missing.reduce((a, b) => a + b);
+//   return sumNormal - sumWithInitial;
+// }
+// console.log(findMissing([1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 5, 6]));
+
+// 1 çözümt bul = (array) => {
+//     array = array.sort((a,b) => a-b)
+//     console.log(array)
+//     for(let i in array) {
+//         if(array[i+1] - array[i] != 1){
+//             return array[i] +1
+//         }
+//     }
+// }
+// console.log(bul(1,2,3,4,6,7));
+
+
+
+// let bul2 = (array) => {
+//   array = array.sort((a, b) => a - b);
+//   console.log(array);
+//   if (array[0] != 1) {
+//     return 1;
+//   } else {
+//     for (let i in array) {
+//       if (array[Number(i) + 1] - array[Number(i)] != 1) {
+//         return Number(array[i] + 1);
+//       }
+//     }
+//   }
+// };
+// console.log(bul2([10, 2, 3, 5, 6, 8, 7, 9, 4]));
+
+
+
+
+//? 4) verilen dizide hangi elemanın kaç defa geçtiğini çıktı veren fonksiyon?
+
+
+// let count = (dizi) => {
+//   let ilk = [];
+//   let son = [];
+//   dizi.forEach((element) => {
+//     let count = 0;
+//     let eleman = "";
+//     for (let item of dizi) {
+//       if (element === item) {
+//         count++;
+//         eleman = item;
+//       }
+//     }
+//     ilk = [eleman, count];
+//     if (!son.length) {
+//       son = son.concat(ilk);
+//     } else {
+//       let counter = 0;
+//       for (let i = 0; i < son.length; i += 2) {
+//         if (son[i] !== eleman) {
+//           counter++;
+//         }
+//       }
+//       if (counter * 2 == son.length) {
+//         son = son.concat(ilk);
+//       }
+//     }
+//   });
+//   return son;
+// };
+// let dizi1 = ["ali", "veli", "ali", "veli", "veli", "2", 2, 2, 2];
+// console.log(count(dizi1));
