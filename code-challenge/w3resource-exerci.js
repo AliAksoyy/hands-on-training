c2c83028ce009e90604b46a87af7bc48d49ccdcb
Ali <alifeyza4017@gmail.com>
2022-09-06 12:41:06 +0300

    added

--- code-challenge/w3resource-exerci.js
+++ code-challenge/w3resource-exerci.js
@@ -1075,20 +1075,20 @@
 // " 24"
 // "row 1"
 
-// const a = [
-//   [1, 2, 1, 24],
-//   [8, 11, 9, 4],
-//   [7, 0, 7, 27],
-//   [7, 4, 28, 14],
-//   [3, 10, 26, 7],
-// ];
+const a = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+];
 
-// for (const i in a) {
-//   console.log(`row ${i}`);
-//   for (const j in a[i]) {
-//     console.log(` ${a[i][j]}`);
-//   }
-// }
+for (const i in a) {
+  console.log(`row ${i}`);
+  for (const j in a[i]) {
+    console.log(` ${a[i][j]}`);
+  }
+}
 
 // 2.çözüm
 
