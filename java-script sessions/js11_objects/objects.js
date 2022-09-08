b5e910f8ee880cba67812f87a3c751d32aec3574
Ali <alifeyza4017@gmail.com>
2022-09-08 07:59:42 +0300

    added

--- java-script sessions/js11_objects/objects.js
+++ java-script sessions/js11_objects/objects.js
@@ -279,3 +279,6 @@ console.log(avgAges);
 
 
 
+const nestedArray = [1, 2, 3, [4, 5, 6], 7];
+const [first, second, third, [fourth, fifth, sixth], seventh] = nestedArray;
+// first = 1, second = 2, third = 3, fourth = 4, fifth = 5, sixth = 6, seventh 
