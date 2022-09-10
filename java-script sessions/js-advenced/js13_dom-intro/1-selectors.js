233223cc2e873d9870efc246ed211a35d0a77f2e
Ali <alifeyza4017@gmail.com>
2022-09-10 13:36:49 +0300

    added

--- java-script sessions/js-advenced/js13_dom-intro/1-selectors.js
+++ java-script sessions/js-advenced/js13_dom-intro/1-selectors.js
@@ -81,3 +81,6 @@ const addItem = document.querySelector(".add-item")
 console.log(addItem.children)
 const h2 = addItem.children[0]
 h2.style.color = "blue"
+
+console.log(addItem.parentNode.parentNode.children[0])
+
