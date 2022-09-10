87d1ec3fa7219df1f29941fe676cf6437d9d8715
Ali <alifeyza4017@gmail.com>
2022-09-10 12:25:22 +0300

    added

--- java-script sessions/js-advenced/js13_dom-intro/1-selectors.js
+++ java-script sessions/js-advenced/js13_dom-intro/1-selectors.js
@@ -6,3 +6,28 @@ console.log("***** SELECTORS *****");
 
 //* EXAMPLE-1
 //*-------------------------------------------
+
+
+      const h1 = document.getElementById("header");
+      console.log(h1);
+      h1.style.color = "white";
+      h1.style.background = "red"
+
+     const myinput =  document.getElementById("input")
+      const button = document.getElementById("btn")
+
+      button.style.background = "black",
+      button.style.color = "white";
+      button.style.width = "7rem";
+      button.style.borderRadius = "10px";
+      button.style.padding = "1rem";
+      myinput.style.padding = "1rem";
+
+      const list = document.getElementsByTagName("li");
+      console.log(list)
+      list[2].style.color = "red"
+      const elementThree = list.item(3);
+      elementThree.style.color = "blue"
+      elementThree.textContent = "React / Vue / Angular"
+
+      list[4].innerText = "Django /Flask"
