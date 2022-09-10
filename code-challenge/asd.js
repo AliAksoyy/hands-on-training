89451b60e98fd7604604172cd50fbcc965f9dfd8
Ali <alifeyza4017@gmail.com>
2022-09-10 16:26:58 +0300

    added

--- code-challenge/asd.js
+++ code-challenge/asd.js
@@ -20,33 +20,33 @@
         
 //     }
 
-    function degisim (num) {
-        const romanRaklam = {
-            M:1000,
-            CM:900,
-            D:500,
-            CD:400,
-            C:100,
-            XC:90,
-            L:50,
-            XL:40,
-            X:10,
-            IX:9,
-            V:5,
-            IV:4,
-            I:1,
-        }
-        let empty = []
-        for(let roman of Object.keys(romanRaklam)) {
-            while(num>=romanRaklam[roman]) {
-                empty.push(roman)
-                num -=romanRaklam[roman]
+    // function degisim (num) {
+    //     const romanRaklam = {
+    //         M:1000,
+    //         CM:900,
+    //         D:500,
+    //         CD:400,
+    //         C:100,
+    //         XC:90,
+    //         L:50,
+    //         XL:40,
+    //         X:10,
+    //         IX:9,
+    //         V:5,
+    //         IV:4,
+    //         I:1,
+    //     }
+    //     let empty = []
+    //     for(let roman of Object.keys(romanRaklam)) {
+    //         while(num>=romanRaklam[roman]) {
+    //             empty.push(roman)
+    //             num -=romanRaklam[roman]
 
-            }
-        }
-        return empty.join("")
-    }
-    console.log(degisim(12))
+    //         }
+    //     }
+    //     return empty.join("")
+    // }
+    // console.log(degisim(12))
    
     
         
