1ca4467063bd571cf06d23ecab634d3f7f888a0d
Ali <alifeyza4017@gmail.com>
2022-09-12 21:16:32 +0300

    added

--- java-script sessions/js-advenced/js14_events/1-events-intro/events.js
+++ java-script sessions/js-advenced/js14_events/1-events-intro/events.js
@@ -7,9 +7,35 @@ console.log("****** EVENTS *******");
 //* EXAMPLE-1 (Mouse Over, Mouse Out )
 //* -------------------------------------------------------
 const h1 =document.querySelector("#header h1")
-console.log(h1)
+h1.onmouseover = function() {
+h1.style.color = "red";
+h1.style.fontWeight = "200"
+}
+
+h1.onmouseout = () => {
+h1.style.color = "black";
+h1.style.fontweight = "800"
+}
+
+
+h1.addEventListener("click", ()=> {
+    alert("h1 pressed")
+   
+})
 
 
 
 //* EXAMPLE-2 (addEventListener())
 //* -------------------------------------------------
+document.getElementById("btn").addEventListener("click", function() {
+    const input = document.querySelector("#input")
+    // if(!input.value) {
+    //     alert("Please enter an item")
+    // } else {
+    //     alert(`${input.value} entered`)
+    // }
+    !input.value
+      ? alert("Please enter an item")
+      : alert(`${input.value} entered`);
+      input.value = ""
+})
