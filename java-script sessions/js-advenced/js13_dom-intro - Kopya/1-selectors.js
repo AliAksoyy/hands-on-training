d5e1474423319ecb7a2fe5e97fbcbdd533c2a89e
Ali <alifeyza4017@gmail.com>
2022-09-12 10:03:15 +0300

    added

--- java-script sessions/js-advenced/js13_dom-intro - Kopya/1-selectors.js
+++ java-script sessions/js-advenced/js13_dom-intro - Kopya/1-selectors.js
@@ -197,3 +197,11 @@
 // console.log(ul.firstElementChild.innerText)
 // console.log(ul.lastElementChild.textContent)
 
+const newP =document.createElement("p")
+newP.id = "new-par"
+newP.setAttribute("class", "new-class")
+const text = document.createTextNode("This is a new paragrap")
+newP.appendChild(text)
+console.log(newP)
+const h2 =document.querySelector(".item-list h2")
+h2.after(newP)
