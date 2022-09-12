cf11a18f9bf8ec96c6302558da4d85b51b5a7e20
Ali <alifeyza4017@gmail.com>
2022-09-12 22:45:40 +0300

    added

--- java-script sessions/js-advenced/js14_events/2-event-example/app.js
+++ java-script sessions/js-advenced/js14_events/2-event-example/app.js
@@ -1,1 +1,16 @@
 console.log("**** app.js *******");
+
+const langInput = document.querySelector(".lang-input");
+const addBtn = document.querySelector("#add-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const langList = document.getElementById("lang-list");
+const newUl = document.createElement("ul");
+langList.appendChild(newUl);
+addBtn.addEventListener("click", () => {
+  if (!langInput.value) {
+    alert("Please enter a language");
+  } else {
+    newUl.innerHTML += `<li>${langInput.value}</li>`;
+    langInput.value = "";
+  }
+});
