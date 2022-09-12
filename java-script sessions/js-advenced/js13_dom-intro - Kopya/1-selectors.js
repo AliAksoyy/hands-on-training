b037f5f3a485e559f8797f8dbfcac201d9dbc5d6
Ali <alifeyza4017@gmail.com>
2022-09-12 06:54:35 +0300

    added

--- java-script sessions/js-advenced/js13_dom-intro - Kopya/1-selectors.js
+++ java-script sessions/js-advenced/js13_dom-intro - Kopya/1-selectors.js
@@ -12,7 +12,8 @@
 // h1.style.color = "white";
 // h1.style.background = "red";
 
-
+const h1 = document.getElementById("header")
+console.log(h1)
 
 // //* EXAMPLE-2
 // //*-------------------------------------------
