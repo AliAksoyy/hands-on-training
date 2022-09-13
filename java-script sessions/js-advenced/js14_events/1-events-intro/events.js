a31635de5e6f1ff950f9bbeb0e9dbf10f9cda981
Ali <alifeyza4017@gmail.com>
2022-09-13 16:47:43 +0300

    added

--- java-script sessions/js-advenced/js14_events/1-events-intro/events.js
+++ java-script sessions/js-advenced/js14_events/1-events-intro/events.js
@@ -7,3 +7,12 @@ h1.onmouseout = function () {
     h1.style.color = "black";
     h1.style.fontWeight = "800"
 }
+
+h1.addEventListener("click", function () {
+     alert("h1 pressed")
+})
+
+document.getElementById("btn").addEventListener("click", function () {
+    const input = document.querySelector("#input")
+    if(input.value)
+})
