ca65fa0f3c96e982d03ee5e2f882752379e2ce82
Ali <alifeyza4017@gmail.com>
2022-09-13 20:45:12 +0300

    added

--- java-script sessions/js-advenced/js15-dom-project-example/game.js
+++ java-script sessions/js-advenced/js15-dom-project-example/game.js
@@ -2,3 +2,18 @@
 //*  GUESS MY NUMBER
 //*  Game Logic
 //*======================================
+
+const randomNumber = Math.trunc(Math.random() * 100) +1
+console.log(randomNumber)
+
+document.querySelector(".check-btn").addEventListener("click", () => {
+    const guessInput = Number(document.querySelector(".guess-input").value);
+    const msg = document.querySelector(".msg")
+    const body = document.querySelector("body")
+    if(!guessInput) {
+        msg.innerText = "Please enter a number"
+    }else if (randomNumber === guessInput) {
+        msg.innerText = "Congrats You Win"
+        body.className = "bg-success"
+    }
+})
