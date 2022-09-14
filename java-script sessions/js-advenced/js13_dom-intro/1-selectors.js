7a6a06a130c20bd2ede813d72aa89d420e0e84f9
Ali <alifeyza4017@gmail.com>
2022-09-14 10:40:15 +0300

    added

--- java-script sessions/js-advenced/js13_dom-intro/1-selectors.js
+++ java-script sessions/js-advenced/js13_dom-intro/1-selectors.js
@@ -3,3 +3,6 @@ const h1 = document.getElementById("header")
 h1.style.color = "white"
 h1.style.background = "red"
 console.log(h1)
+
+document.getElementById("input").style.background = "black"
+document.getElementById("btn").style.color = "white"
