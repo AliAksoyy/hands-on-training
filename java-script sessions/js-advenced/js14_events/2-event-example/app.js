dcfc0dd67692b8e9878efda7c6af51b85eb06b75
Ali <alifeyza4017@gmail.com>
2022-09-14 13:38:34 +0300

    added

--- java-script sessions/js-advenced/js14_events/2-event-example/app.js
+++ java-script sessions/js-advenced/js14_events/2-event-example/app.js
@@ -8,11 +8,22 @@ const langList = document.querySelector("#lang-list")
 const newUl =document.createElement("ul")
 langList.appendChild(newUl);
 
+window.addEventListener("load", ()=> {
+  langInput.focus()
+})
+
 addBtn.addEventListener("click", () => {
   if(!langInput.value) {
     alert("please enter a language")
   }else {
     newUl.innerHTML += `<li>${langInput.value}</li>`;
     langInput.value = ""
+  langInput.focus();
+
   }
+})
+
+deleteBtn.addEventListener("click", function () {
+  newUl.childElementCount >0 ?
+  newUl.removeChild(newUl.lastElementChild) : alert("There is no item to delete")
 })
