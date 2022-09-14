9ec6ebc2361cc03a852f1495a67cd6861f6c4f50
Ali <alifeyza4017@gmail.com>
2022-09-14 11:30:47 +0300

    added

--- java-script sessions/js-advenced/js13_dom-intro/2-traversingDOM.js
+++ java-script sessions/js-advenced/js13_dom-intro/2-traversingDOM.js
@@ -2,3 +2,15 @@ const list = document.querySelector(".list")
 console.log(list.parentNode)
 const  ul = list.parentNode
 console.log(list.parentElement)
+
+
+console.log(list.closest("main"))
+
+const addItem = document.querySelector(".add-item")
+console.log(addItem.children)
+
+// const h2 = addItem.children[0]
+console.log(addItem.firstChild)
+// h2.getElementsByClassName.color = "blue"
+
+console.log(addItem.parentNode.parentNode.children[0])
