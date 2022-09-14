5af26fd3759f98f1479aa64e37e16f8148173994
Ali <alifeyza4017@gmail.com>
2022-09-14 10:52:07 +0300

    added

--- java-script sessions/js-advenced/js13_dom-intro/1-selectors.js
+++ java-script sessions/js-advenced/js13_dom-intro/1-selectors.js
@@ -6,3 +6,17 @@ console.log(h1)
 
 document.getElementById("input").style.background = "black"
 document.getElementById("btn").style.color = "white"
+
+
+const list = document.getElementsByTagName("li")
+console.log(list)
+list[2].style.color = "red"
+list.item(3).style.color = "blue"
+const elementThree = list.item(3)
+elementThree.textContent = "react / vue / angular"
+
+list[4].innerText = "Django / Flask"
+console.log(list[1].textContent)
+console.log(list[1].innerText)
+list[4].innerHTML = '<a href="www.https://clarusway.com">ClarusWay Web</a>'
+console.log(list[4])
