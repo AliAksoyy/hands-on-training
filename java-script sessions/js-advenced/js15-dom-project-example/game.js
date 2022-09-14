4453e940cc62971ba49654b7d5c5e2b7c2f6940a
Ali <alifeyza4017@gmail.com>
2022-09-14 10:08:51 +0300

    added

--- java-script sessions/js-advenced/js15-dom-project-example/game.js
+++ java-script sessions/js-advenced/js15-dom-project-example/game.js
@@ -1,31 +1,3 @@
-//* =====================================
-//*  GUESS MY NUMBER
-//*  Game Logic
-//*======================================
-
-const randomNumber = Math.trunc(Math.random() * 100) +1
-console.log(randomNumber)
-
-let score = 10
-let topScore = 0
-
-document.querySelector(".check-btn").addEventListener("click", () => {
-    const guessInput = Number(document.querySelector(".guess-input").value);
-    const msg = document.querySelector(".msg")
-    const body = document.querySelector("body")
-    if(!guessInput) {
-        msg.innerText = "Please enter a number"
-    }else if (randomNumber === guessInput) {
-        msg.innerText = "Congrats You Win"
-        body.className = "bg-success"
-        if(score>topScore) {
-            topScore = score;
-            document.querySelector(".top-score").textContent = topScore
-        }
-        document.querySelector(".secret-number").textContent = randomNumber
-    } else {
-        score--
-        guessInput > randomNumber ? (msg.innerText = "decrease") : (msg.innerText = "decrease")
-        document.querySelector(".score").textContent =score
-    }
-})
+myObj = {a:1,b:2, c:3}
+localStorage.setItem("OBJ", JSON.stringify(myObj))
+localStorage.setItem("OBJ")
