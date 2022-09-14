d529e4d199eeacec2632a857b67402d08e7d8b99
Ali <alifeyza4017@gmail.com>
2022-09-14 13:25:54 +0300

    added

--- java-script sessions/js-advenced/js14_events/2-event-example/app.js
+++ java-script sessions/js-advenced/js14_events/2-event-example/app.js
@@ -1,1 +1,9 @@
 
+const langInput = document.querySelector(".lang-input")
+const addBtn = document.querySelector("#add-btn")
+const deleteBtn = document.querySelector("#delete-btn")
+const langList = document.querySelector("#lang-list")
+
+
+const newUl =document.createElement("ul")
+langList.appendChild(newUl)
