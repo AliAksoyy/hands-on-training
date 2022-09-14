02fed5b1cd37a92203909688cac6a4890e25fb91
Ali <alifeyza4017@gmail.com>
2022-09-14 12:00:55 +0300

    added

--- java-script sessions/js-advenced/js13_dom-intro/3-createNode.js
+++ java-script sessions/js-advenced/js13_dom-intro/3-createNode.js
@@ -10,3 +10,12 @@ console.log(newP)
 const h2 = document.querySelector(".item-list h2")
 
 h2.after(newP)
+
+console.log(newP.getAttribute("class"))
+console.log(newP.getAttribute("id"))
+
+console.log(newP.classList.contains("par"))
+newP.classList.add("newP")
+newP.classList.toggle("newp")
+console.log(newP)
+
