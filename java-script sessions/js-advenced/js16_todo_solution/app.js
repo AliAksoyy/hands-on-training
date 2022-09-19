1e07376cfbda4b0a087e3e9cf194ffcf2319552d
Ali <alifeyza4017@gmail.com>
2022-09-19 20:39:13 +0300

    added

--- java-script sessions/js-advenced/js16_todo_solution/app.js
+++ java-script sessions/js-advenced/js16_todo_solution/app.js
@@ -1,3 +1,18 @@
 //* ======================================================
 //*                     TODO APP
 //* ======================================================
+// selectors
+const addBtn = document.querySelector("#todo-button")
+const todoInput = document.getElementById("todo-input")
+const todoUl = document.querySelector("#todo-ul")
+
+// evetn add
+
+addBtn.addEventListener("click", function() {
+
+    if(todoInput.value.trim() === "") {
+        alert("Please enter new ToDo")
+    }else {
+        console.log(todoInput.value)
+    }
+})
