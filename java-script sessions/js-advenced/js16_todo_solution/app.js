85f8517110fb6175014cc7e03ddcb36e800c4018
Ali <alifeyza4017@gmail.com>
2022-09-19 21:22:52 +0300

    added

--- java-script sessions/js-advenced/js16_todo_solution/app.js
+++ java-script sessions/js-advenced/js16_todo_solution/app.js
@@ -1,18 +1,45 @@
 //* ======================================================
 //*                     TODO APP
 //* ======================================================
-// selectors
-const addBtn = document.querySelector("#todo-button")
-const todoInput = document.getElementById("todo-input")
-const todoUl = document.querySelector("#todo-ul")
 
-// evetn add
+//?Selectors
+const addBtn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
 
-addBtn.addEventListener("click", function() {
+addBtn.addEventListener("click", () => {
+  if (todoInput.value.trim() === "") {
+    alert("PLease enter new todo");
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
 
-    if(todoInput.value.trim() === "") {
-        alert("Please enter new ToDo")
-    }else {
-        console.log(todoInput.value)
-    }
-})
+    createListElement(newTodo);
+  }
+});
+
+const createListElement = (newTodo) => {
+  //? yeni bir li elementi olustur ve bu elemente obje icerisindeki
+  //? id degerini ve completed class'ini ata
+  const li = document.createElement("li");
+  // li.id = newTodo.id;
+  li.setAttribute("id", newTodo.id);
+
+  //? okey ikonu olustur ve li elementine bagla
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+
+  //? todo basligi icin bir p elementi ve yazi dugumu olusturarak li'ye bagla
+    const p = document.createElement("p")
+    const pTextNode = document.createElement(newTodo.text)
+    p.append(pTextNode)
+    li.appendChild(p)
+  //? delete ikonu olustur ve li elementine bagla
+  console.log(li);
+
+  todoUl.appendChild(li);
+};
