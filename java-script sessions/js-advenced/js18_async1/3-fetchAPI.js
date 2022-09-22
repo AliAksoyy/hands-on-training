e44f34d6e59147df8edfbe92a62a1bfca122cfc0
Ali <alifeyza4017@gmail.com>
2022-09-22 23:30:56 +0300

    added

--- java-script sessions/js-advenced/js18_async1/3-fetchAPI.js
+++ java-script sessions/js-advenced/js18_async1/3-fetchAPI.js
@@ -11,7 +11,7 @@
 //? bir parametre almaktadir ve bu istegin cevabini gosteren bir Promise dondurmektedir.
 
 console.log("FETCH");
-
+// let userdata;
 fetch("https://api.github.com/users")
 .then((res)=> {
     console.log(res)
@@ -19,8 +19,11 @@ fetch("https://api.github.com/users")
         throw new Error("Something went wrong")
     }
    return res.json()})
-.then((data)=> console.log(data))
+.then((data)=> (updateDOM(data)))
 .catch((err)=> console.log(err))
 
+const updateDom = (users)=> {
+    console.log(users)
+}
 const userDiv = document.querySelector(".users")
 
