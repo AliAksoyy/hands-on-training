c8ba95ad7abf4aa96b92c2389daef71e69c2e857
Ali <alifeyza4017@gmail.com>
2022-09-22 23:01:17 +0300

    added

--- java-script sessions/js-advenced/js18_async1/3-fetchAPI.js
+++ java-script sessions/js-advenced/js18_async1/3-fetchAPI.js
@@ -21,3 +21,6 @@ fetch("https://api.github.com/users")
    return res.json()})
 .then((data)=> console.log(data))
 .catch((err)=> console.log(err))
+
+const userDiv = document.querySelector(".users")
+
