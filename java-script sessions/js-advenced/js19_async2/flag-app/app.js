5a1b24088b06cd5c0dc5af1ea1be5ff35a4e2e78
Ali <alifeyza4017@gmail.com>
2022-09-24 14:20:40 +0300

    added

--- java-script sessions/js-advenced/js19_async2/flag-app/app.js
+++ java-script sessions/js-advenced/js19_async2/flag-app/app.js
@@ -27,10 +27,12 @@ const renderError = () => {
 const renderCountries = (data) => {
   console.log(data);
   const {capital, currencies, flags:{svg},languages, name: {common}} =data[0]
+
+  console.log(Object.values(languages))
   const countryDiv = document.querySelector(".countries");
 
   countryDiv.innerHTML = `
-    <div class="card" style="width: 18rem;">
+    <div class="card mx-auto m-3 shadow-lg" style="width: 18rem;">
       <img src="${svg}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${common}</h5>
@@ -40,8 +42,14 @@ const renderCountries = (data) => {
         <li class="list-group-item">An item
         <i class="fas fa-lg fa-landmark"></i>${capital}
         </li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">
+        <i class="fa-solid fa-comments"></i> ${Object.values(languages)}
+        </li>
+        <li class="list-group-item">
+        <i class="fas fa-lg fa-money-bill-wave"></i> ${
+          Object.values(currencies)[0].name
+        }, ${Object.values(currencies)[0].symbol}
+        </li>
       </ul>
       <div class="card-body">
         <a href="#" class="card-link">Card link</a>
