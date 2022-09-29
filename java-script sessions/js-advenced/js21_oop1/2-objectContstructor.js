998fc257c37bdc2d50b33e2d7bf0bbe9d1452e82
Ali <alifeyza4017@gmail.com>
2022-09-29 21:17:01 +0300

    added

--- java-script sessions/js-advenced/js21_oop1/2-objectContstructor.js
+++ java-script sessions/js-advenced/js21_oop1/2-objectContstructor.js
@@ -10,4 +10,26 @@
 
 //? Object Constructor
 
+function Book (title,author,year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.getSummary = function() {
+        return `${this.title} was written by ${this.author} in ${this.year}`
+    }
+}
+
+const book1 = new Book("Kasagi", "omer seyfettin", 1920,"ali")
+const book2 = new Book("sinekli bakkal","H.edip adıvar",1990)
+
+console.log(book1.getSummary())
+
+book1.price =100
+console.log(book1)
+
+Book.prototype.getAge = function () {
+    return new Date().getFullYear() -this.year
+}
+console.log(book1,book2)
+console.log(book1.getAge())
 
