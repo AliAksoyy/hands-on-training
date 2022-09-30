6a28ada8b9534d37715b4423a808272a1e17fba2
Ali <alifeyza4017@gmail.com>
2022-09-30 23:11:32 +0300

    added

--- java-script sessions/js-advenced/js17_checkout_solution/checkout.js
+++ java-script sessions/js-advenced/js17_checkout_solution/checkout.js
@@ -10,3 +10,42 @@
 // })
 
 
+document.querySelector(".products").addEventListener("click",(e)=> {
+
+  if(e.target.className == "fa-solid fa-plus") {
+    e.target.previousElementSibling.innerText++
+    hesapla(e.target)
+    lineHesapla();
+
+  }else if(e.target.classList.contains("fa-minus")) {
+    if (e.target.nextElementSibling.innerText > 1) {
+      e.target.nextElementSibling.innerText--;
+    }else {
+      if(confirm("Are you sure")) {
+        e.target.closest(".product").remove()
+      }
+    }
+     hesapla(e.target);
+     lineHesapla();
+  }else if(e.target.className == "remove-product") {
+    e.target.closest(".product").remove()
+    lineHesapla()
+  }
+})
+
+function hesapla(btn) {
+  const product = btn.parentElement.parentElement.parentElement
+
+
+  
+}
+
+
+
+
+
+
+const lineHesapla = function() {
+
+
+}
