21a9cf338cf7530f9f36f47f298bd90e684c6c47
Ali <alifeyza4017@gmail.com>
2022-10-02 06:46:29 +0300

    added

---
+++ java-script sessions/js-advenced/js22_oop2-modules/modules/mymodule.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+import karesi, {
+    WHW as W,
+    WORKING_HOURS_WEEK,
+    dec,
+    inc,
+    sum,
+    counter,
+}from "./mymodule.js"
+
+console.log('W:>>', W)
+console.log('karesi(4):>>', karesi(4))
