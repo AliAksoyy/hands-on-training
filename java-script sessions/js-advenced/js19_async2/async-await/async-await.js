19f84b894b70537eb9fdfae8778235016ec4a941
Ali <alifeyza4017@gmail.com>
2022-10-02 08:15:45 +0300

    added

--- java-script sessions/js-advenced/js19_async2/async-await/async-await.js
+++ java-script sessions/js-advenced/js19_async2/async-await/async-await.js
@@ -18,52 +18,88 @@
 //* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
 //* degerlerinin dondurulmesine ile kodun calismasi devam eder.
 
-let isError = false;
+
+const getNews = async()=> {
+const apiKey = "91bebcb7e4a2421089c507605565fcf0";
+
+  const url = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${apiKey}`;
+
+const res = await fetch(url);
+
+const data = await res.json()
+renderFetch(data)
+}
+
+const renderFetch = function (countries) {
+ console.log(countries)
+}
 
 
-const getNews = async function () {
-  const API_KEY = "55e3f1fdc1c94581b6b11537c9b8f8bf";
-  const url =
-    "https://newsapi.org/v2/top-headlines?country=tr&apiKey=" + API_KEY;
 
-  try {
-    const res = await fetch(url);
-    if (!res.ok) {
-      isError = true;
-      // throw new Error(`Something went wrong: ${res.status} `);
-    }
-    const data = await res.json();
-    // console.log(data.articles);
-    renderNews(data.articles);
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-const renderNews = (news) => {
-  const newsList = document.getElementById("news-list");
-  if (isError) {
-    newsList.innerHTML += `
-      <h2>News Can not be fetched</h2>
-      <img src="./img/404.png" alt="" />
-    `;
-    return;
-  }
-  news.forEach((item) => {
-    const { title, description, urlToImage, url } = item; //! dest
-    newsList.innerHTML += `
-    <div class="col-md-6 col-lg-4 col-xl-3">
-      <div class="card">
-        <img src="${urlToImage}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text">${description}</p>
-          <a href="${url}" target="_blank" class="btn btn-danger">Details</a>
-        </div>
-      </div>
-    </div>
-    `;
-  });
-};
 
-window.addEventListener("load", getNews);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let isError = false;
+
+
+// const getNews = async function () {
+//   const API_KEY = "55e3f1fdc1c94581b6b11537c9b8f8bf";
+//   const url =
+//     "https://newsapi.org/v2/top-headlines?country=tr&apiKey=" + API_KEY;
+
+//   try {
+//     const res = await fetch(url);
+//     if (!res.ok) {
+//       isError = true;
+//       // throw new Error(`Something went wrong: ${res.status} `);
+//     }
+//     const data = await res.json();
+//     // console.log(data.articles);
+//     renderNews(data.articles);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// const renderNews = (news) => {
+//   const newsList = document.getElementById("news-list");
+//   if (isError) {
+//     newsList.innerHTML += `
+//       <h2>News Can not be fetched</h2>
+//       <img src="./img/404.png" alt="" />
+//     `;
+//     return;
+//   }
+//   news.forEach((item) => {
+//     const { title, description, urlToImage, url } = item; //! dest
+//     newsList.innerHTML += `
+//     <div class="col-md-6 col-lg-4 col-xl-3">
+//       <div class="card">
+//         <img src="${urlToImage}" class="card-img-top" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">${title}</h5>
+//           <p class="card-text">${description}</p>
+//           <a href="${url}" target="_blank" class="btn btn-danger">Details</a>
+//         </div>
+//       </div>
+//     </div>
+//     `;
+//   });
+// };
+
+// window.addEventListener("load", getNews);
