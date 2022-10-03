ba98b447a3e2a5c65ac7c3e0964e8506c000a903
Ali <alifeyza4017@gmail.com>
2022-10-03 06:55:08 +0300

    added

--- java-script sessions/js-advenced/js20_weather_app_solution/weather.js
+++ java-script sessions/js-advenced/js20_weather_app_solution/weather.js
@@ -4,6 +4,23 @@ const msg = document.querySelector("span.msg")
 const list = document.querySelector(".ajax-section ul")
 
 
+localStorage.setItem("tokenKey", "5bb1e360c88ace5e3a7f0c8cfe9a34d7")
+
+
+
+
+form.addEventListener("submit", (e)=> {
+  e.preventDefault()
+  getWeatherDataFromApi()
+})
+
+const getWeatherDataFromApi = () => {
+ console.log("ali")
+
+
+}
+
+
 
 
 
