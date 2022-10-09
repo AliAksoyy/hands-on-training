41c5fd9931ab1df56a6aba11fde1d316699c4d28
Ali <alifeya4017@gmail.com>
2022-10-09 22:14:11 +0300

    added

--- react-sessions/rc02_jsx_comp_basicStyling/src/index.js
+++ react-sessions/rc02_jsx_comp_basicStyling/src/index.js
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import App from "./App";
+import App from "./App";
 import "./index.css";
-const a = 5
-const El = () => {
-    return (
-    <div>
 
-<h1>Hello {a}</h1>
-<p>Ali</p>
-</div>)
-}
 
 //! index componenti ReactDOM.createRoot() metodu ile
 //! public klasorunde yer alan index.html icersindeki id'si root
@@ -19,4 +11,4 @@ const El = () => {
 
 //* render() metodu içerisinde App componenti cagrilir.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<El />);
+root.render(<App />);
