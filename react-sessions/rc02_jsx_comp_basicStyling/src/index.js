18b5f261ab80fbd543a505ae2126d0608f050b78
AliAksoyy <alifeyza4017@gmail.com>
2022-10-10 08:45:01 +0300

    added-

--- react-sessions/rc02_jsx_comp_basicStyling/src/index.js
+++ react-sessions/rc02_jsx_comp_basicStyling/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-
 //! index componenti ReactDOM.createRoot() metodu ile
 //! public klasorunde yer alan index.html icersindeki id'si root
 //! olan elemana erisir.
