b237ec201d263518c9fb0194d00203551cf0ba54
AliAksoyy <alifeyza4017@gmail.com>
2022-10-10 11:37:21 +0300

    added

--- react-sessions/rc02_props/src/App.js
+++ react-sessions/rc02_props/src/App.js
@@ -11,21 +11,16 @@ function App() {
   return (
     
     <>
-    <Person 
-    name={data[0].name} 
-    img={data[0].img}
-    tel = {data[0].tel} />
-    <Person 
-    name={data[1].name} 
-    img={data[1].img}
-    tel = {data[1].tel} />
-    <Person 
-    name={data[2].name} 
-    img={data[2].img}
-    tel = {data[2].tel} />
-     
+    {data.map((person)=> <Person
+    key= {person.name}
+    name={person.name} 
+    img = {person.img} 
+    tel= {person.tel} />
+
+    )}
     </>
     )
 }
 
 export default App;
+
