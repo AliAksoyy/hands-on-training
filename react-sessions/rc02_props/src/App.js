8de98d41387a7e9da7ca321ec47edb3c2121a297
AliAksoyy <alifeyza4017@gmail.com>
2022-10-10 10:33:27 +0300

    added

--- react-sessions/rc02_props/src/App.js
+++ react-sessions/rc02_props/src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
-// import Msg from "./components/Msg";
-import Person from "./components/Person";
-import data from "./data";
+import Msg from "./Component/Msg";
+
+// import Person from "./components/Person";
+// import data from "./data";
 
 function App() {
   return (
-    <div>
-      {data.map((person) => (
-        <Person
-          key={person.name}
-          name={person.name}
-          img={person.img}
-          tel={person.tel}
-        />
-      ))}
-    </div>
-  );
+    <Msg />
+  )
 }
 
 export default App;
---
+++ react-sessions/rc02_props/src/Component/Msg.jsx
@@ -0,0 +1,9 @@
+function Msg ()  {
+    return (
+        <div>
+            <h3>Merhaba Felix</h3>
+        </div>
+    )
+}
+
+export default Msg;
