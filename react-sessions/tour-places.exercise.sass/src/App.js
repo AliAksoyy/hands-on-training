785cd51ec3c6994804cea6c97687c274550f1d1a
AliAksoyy <alifeyza4017@gmail.com>
2022-10-11 20:33:27 +0300

    added

--- react-sessions/tour-places.exercise.sass/src/App.js
+++ react-sessions/tour-places.exercise.sass/src/App.js
@@ -1,12 +1,14 @@
 
 import "./App.scss"
 
-function App() {
-  return (
+const App = function() {
+
+  return(
     <div className="App">
-      
+      <h1>Ali Akosy</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit atque iste culpa sint fuga molestias ab non dolores ipsa eaque!</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
