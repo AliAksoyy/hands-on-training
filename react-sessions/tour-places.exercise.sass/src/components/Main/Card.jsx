d5fae443293abbe658390ebd07f27af84e3af573
AliAksoyy <alifeyza4017@gmail.com>
2022-10-11 21:42:20 +0300

    added

---
+++ react-sessions/tour-places.exercise.sass/src/components/Main/Card.jsx
@@ -0,0 +1,17 @@
+
+
+const Card = (item) => {
+
+    return (
+        <div className="cards">
+        <div className="title">
+            <h1>{item.title}</h1>
+        </div>
+           <img src={item.image} alt="img" />
+        <div className="card-over">
+           <p>{item.desc}</p>
+        </div>
+        </div>
+    )
+}
+export default Card
--- react-sessions/tour-places.exercise.sass/src/components/Main/Main.jsx
+++ react-sessions/tour-places.exercise.sass/src/components/Main/Main.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import "./Main.scss";
 import { data } from "../../helper/data";
+import Card from "./Card"
 
 export class Main extends React.Component {
     
     render() {
-        console.log(data)
+    
         return(
             <div className="card-container">
-                {data.map((item)=> {
-                    return(
-                        <div className="cards">
-                            <h1>{item.title}</h1>
-                        </div>
-                    )
-                })}
+                {data.map((item)=> <Card {...item}/>
+                    
+                )}
             </div>
         )
     }
