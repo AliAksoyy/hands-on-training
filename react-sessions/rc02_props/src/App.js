02548c11a891b06388eea6cc230df70cbc27d18f
AliAksoyy <alifeyza4017@gmail.com>
2022-10-12 15:23:15 +0300

    added

--- react-sessions/rc02_props/src/App.js
+++ react-sessions/rc02_props/src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
+import { Msg as M } from "./component/Msg"
 
 
-function App() {
-  return (
-    <div>
-      
-      ali
-    </div>
-  );
+function App () {
+
+return(
+  <div>
+  <h1>Ali</h1>
+  <M />
+ 
+
+  </div>
+)
+
 }
 
-export default App;
+export default App
---
+++ react-sessions/rc02_props/src/component/Msg.jsx
@@ -0,0 +1,12 @@
+
+
+export const Msg = function() {
+
+return (
+    <div>
+        <h1>Msg</h1>
+    </div>
+)
+
+}
+
