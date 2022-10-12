3c58675e5ab9e8bc39d6ece4b0b35af750928445
AliAksoyy <alifeyza4017@gmail.com>
2022-10-12 20:33:52 +0300

    added

--- react-sessions/rc05_events_useStateHook/src/components/1-events/Events.js
+++ react-sessions/rc05_events_useStateHook/src/components/1-events/Events.js
@@ -2,9 +2,18 @@
 
 const Events = () => {
 
+const handleClick = () => {
+    alert("Btn Clicked")
+}
+
+const handleClear = function(msg) {
+    alert(msg)
+}
+
 return(
-    <div>
-            <button>Click</button>
+    <div className="container text-center mt-4">
+            <button onClick={handleClick} className="btn btn-success">Click</button>
+            <button onClick={()=>handleClear("Clear Btn Clicked")} className="btn btn-dark">Clear</button>
     </div>
 )
 
