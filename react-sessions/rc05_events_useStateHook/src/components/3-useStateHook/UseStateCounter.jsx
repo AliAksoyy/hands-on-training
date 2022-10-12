282aeaccbf301cdf6ff7e7dd8d06ec9577719ad4
AliAksoyy <alifeyza4017@gmail.com>
2022-10-12 22:18:37 +0300

    added

--- react-sessions/rc05_events_useStateHook/src/components/3-useStateHook/UseStateCounter.jsx
+++ react-sessions/rc05_events_useStateHook/src/components/3-useStateHook/UseStateCounter.jsx
@@ -2,6 +2,13 @@
 
 export const UseStateCounter = () => {
     return(
-            <div>as</div>
+        <div className="container text-center mt-4">
+
+        <h1>CLASSFUL COMPONENTS</h1>
+        <h2 className="display-4 text-danger">COUNT:</h2>
+        <button  className="btn btn-success">INC</button>
+        
+        <button  className="btn btn-danger">DEC</button>
+</div>
     )
 }
