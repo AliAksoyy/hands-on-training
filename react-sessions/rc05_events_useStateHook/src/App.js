c3384815865a0b2ad0dab833f40d529459a5077d
AliAksoyy <alifeyza4017@gmail.com>
2022-10-13 06:20:17 +0300

    added

--- react-sessions/rc05_events_useStateHook/src/App.js
+++ react-sessions/rc05_events_useStateHook/src/App.js
@@ -1,15 +1,10 @@
-import Events from "./components/1-events/Events";
-import Counter from "./components/2-classComponent/Counter";
-import { UseStateCounter as Use } from "./components/3-useStateHook/UseStateCounter";
+
 
 function App() {
   return (
     <div>
 
-      <Events />
-      <Counter  />
-      <Counter counter={5} />
-      <Use />
+     Ali
 
     </div>
   );
