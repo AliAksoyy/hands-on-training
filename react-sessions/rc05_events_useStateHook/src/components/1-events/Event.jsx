8a04a1b60d6fe4ed8f5c3c1ca87c3eda0f994dcd
AliAksoyy <alifeyza4017@gmail.com>
2022-10-13 08:27:11 +0300

    added

--- react-sessions/rc05_events_useStateHook/src/components/1-events/Event.jsx
+++ react-sessions/rc05_events_useStateHook/src/components/1-events/Event.jsx
@@ -2,6 +2,8 @@
 
 const Event =()=> {
 
+    let message = "ALİ AKSOY MERHABA"
+
     const handleClick = () => {
         alert("Btn Clicked")
     }
@@ -10,6 +12,7 @@ const Event =()=> {
     }
     const handleChange = function(e) {
         console.log(e.target);
+        message= "BEYDA AKSOYY"
     }
 
 
@@ -17,6 +20,7 @@ const Event =()=> {
     return(
 
         <div className="container mt-4 text-center">
+            <h1>{message}</h1>
             <button onClick={handleClick} className="btn btn-success">Click</button>
             <button onClick={()=>handleClear("Clear Btn Clicked")} className="btn btn-dark" >Clear</button>
             <button onClick={(e)=>handleChange(e)} className="btn btn-danger">Change</button>
