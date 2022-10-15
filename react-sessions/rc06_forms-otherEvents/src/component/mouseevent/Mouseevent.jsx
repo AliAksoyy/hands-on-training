e075d0642f7ff75c6d77e385cd6972070799250f
AliAksoyy <alifeyza4017@gmail.com>
2022-10-15 08:45:56 +0300

    added

--- react-sessions/rc06_forms-otherEvents/src/component/mouseevent/Mouseevent.jsx
+++ react-sessions/rc06_forms-otherEvents/src/component/mouseevent/Mouseevent.jsx
@@ -1,12 +1,33 @@
-
+import { useState } from "react"
 
 const Mouseevent =()=> {
 
+    const [showHint, setShowHint] = useState(false)
+
+    const [toggle, setToggle] = useState(false)
+    const handleDoubleClick = (e) => {
+        setToggle(!toggle)
+        toggle ? (e.target.className = "bg-danger text-light w-50 p-4") : (e.target.className = "bg-danger text-light w-50 p-4")
+    }
+
+    console.log(toggle)
     return(
-        <div className="container text-center d-flex flex-column align-items-center m-4">
+        <div className="container text-center d-flex flex-column align-items-center mt-4">
             <h2 className="text-danger">MOUSE EVENT</h2>
-            <div id="todo-1" className="bg-success text-light w-50 p-4">todo item 1</div>
-            <div id="todo-2" className="bg-success text-light w-50 p-4">todo item 2</div>
+            <div 
+            onMouseEnter={()=> setShowHint(true)}
+             onMouseOut={()=> setShowHint(false)} 
+             id="todo-1" 
+             className="bg-success text-light w-50 p-4"
+             >
+             todo item 1 {showHint && <span>sdasdasdasda</span>}
+             </div>
+            <div 
+            id="todo-2" 
+            className="bg-success text-light w-50 p-4"
+            onDoubleClick={handleDoubleClick}
+            >
+            todo item 2</div>
             <div id="todo-3" className="bg-success text-light w-50 p-4">todo item 3</div>
             <p>X and Y</p>
             <p className="text-danger fw-bold">coordX - coordY</p>
