65e3bf9df78655f20fd0eaee5108023306b79e76
AliAksoyy <alifeyza4017@gmail.com>
2022-10-15 07:51:22 +0300

    added

--- react-sessions/rc06_forms-otherEvents/src/component/keyboard/Keyboard.jsx
+++ react-sessions/rc06_forms-otherEvents/src/component/keyboard/Keyboard.jsx
@@ -3,15 +3,26 @@ import { useState } from "react"
 
 const Keyboard = () => {
     
+    const change = function(e) {
+        console.log(e.keyCode);
+        if(e.keyCode >=48 && e.keyCode <=57) {
+            alert("please not a number")
+            e.preventDefault()
+        }
+    }
 
     const [inputValue, setInputValue] = useState("")
+   
     return(
         <div className="container text-center">
             <h1>Cliipboard</h1>
             <input type="text" 
             className="form-control" 
-            onChange={(e)=> setInputValue(e.target.value)}
-
+            onChange={(e)=> {
+            e.target.value=e.target.value.toLocaleUpperCase();
+            setInputValue(e.target.value)
+            }}
+            onKeyDown={change}
             />
             <p className="text-start mt-4">{inputValue}</p>
         </div>
