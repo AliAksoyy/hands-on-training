f8912f05c4153195b021b782a404aab19a289db2
AliAksoyy <alifeyza4017@gmail.com>
2022-10-15 14:10:49 +0300

    added

--- react-sessions/rc07_language-cards/src/components/Card.jsx
+++ react-sessions/rc07_language-cards/src/components/Card.jsx
@@ -1,13 +1,31 @@
-import Language from "./Language"
+import Container  from "react-bootstrap/Container"
+import Row  from "react-bootstrap/Row";
+import Col  from "react-bootstrap/Col";
+import Language from "./Language";
+import {data} from "../helpers/data"
 
 
 
 function Card() {
+    console.log(data);
     return(
-        <>
-        <h1>card</h1>
-            <Language />
-        </>
+        <Container className="rounded-4 mt-4 p-4" style={{background:"#f48b29"}}>
+        <h1 className="text-white my-4 ">Languages</h1>
+        <Row className="g-3 justify-content-center">
+            
+            {data.map((item,index)=> {
+            return(
+            <Col sm={6} md={4} lg={3} key={index}>
+                <Language  {...item} />
+
+            </Col>
+
+            )
+        })}
+        
+        </Row>
+        
+        </Container>
     )
 }
 
--- react-sessions/rc07_language-cards/src/components/Language.jsx
+++ react-sessions/rc07_language-cards/src/components/Language.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image"
 
+const Language = function({name,img,options}) {
+    
+    const [showDesc, setShowDesc] =useState(false)
 
-const Language = function() {
-
+    console.log(showDesc);
     return(
-        <div>Language</div>
+        <Container style={{background:"peachpuff"}} className="p-4  rounded-2 h-100" onClick={()=> setShowDesc(!showDesc)}>
+
+        {!showDesc && (<Container>
+
+            <Image src={img} width="70%"></Image>
+            <h3 className="mt-2 display-6">{name}</h3>
+
+            </Container>)}
+
+        {showDesc && (
+            <ul >
+                {options.map((item)=> {
+                    return(
+                        <li className="text-start h5">{item}</li>
+                    )
+                })}
+            </ul>
+        )}
+         
+       
+        </Container>
     )
 }
 
