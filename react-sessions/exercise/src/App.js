8a4b97b36c5a79ffa7cb1067cb6d3c4ea4905d14
AliAksoyy <alifeyza4017@gmail.com>
2022-10-17 14:50:04 +0300

    added

--- react-sessions/exercise/src/App.js
+++ react-sessions/exercise/src/App.js
@@ -7,21 +7,29 @@ import "../src/App.css"
 
 const App = () => {
 
-  const [msg, setMsg] = useState("React")
+const [input, setInput] =useState("")
 
- const deg = (e)=> {
-  setMsg(e.target.previousElementSibling.value)
-  
- }
+const [name, setName] = useState("React Learn")
+
+const deg = (e) => {
+    setInput(e.target.value)
+}
+
+const ekle = () => {
+  setName(input)
+  setInput("")
+}
+ 
   
  
 
   return (
     <div>
-    <h2>hello {msg} </h2>
+    <h2>hello {name} </h2>
     <p>isim gir</p>
-    <input type="text"  />
-    <button onClick={deg}> click me</button>
+    <input onChange={deg} type="text"  value={input}/>
+    <button onClick={ekle}>ekle</button>
+    <p>{input}</p>
    </div>
   )
 }
