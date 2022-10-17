fd016fd15596b1bc9ba4ead6a2d66614e34b7a6d
AliAksoyy <alifeyza4017@gmail.com>
2022-10-17 13:01:54 +0300

    added

--- react-sessions/exercise/src/App.js
+++ react-sessions/exercise/src/App.js
@@ -1,24 +1,17 @@
 
 
-import { useState } from "react"
+
 import "../src/App.css"
 
 const App = () => {
 
 
-  const [degerlendirme, setDegerlendirme] = useState(false)
-  
-  const deg = () => {
-    // setValue("beyda")
-    setDegerlendirme(!degerlendirme)
-    console.log(degerlendirme);
-  }
 
   return (
     <div className="App">
-    {degerlendirme ? <h1>ali aksoy</h1> : <h1>Beyda</h1> }
-    
-    <button onClick={deg}>değiştir</button>
+   
+    <input type="text"  onChange={(e)=> console.log(e)}/>
+    <button onClick={()=> alert("ali baba")}>değiştir</button>
       
     </div>
   )
