b36755668ca6a48996453414c3060095e94f450c
AliAksoyy <alifeyza4017@gmail.com>
2022-10-17 12:32:20 +0300

    added

--- react-sessions/exercise/src/App.js
+++ react-sessions/exercise/src/App.js
@@ -1,29 +1,24 @@
 
+
 import { useState } from "react"
 import "../src/App.css"
 
 const App = () => {
-  const [input, setInput] = useState("");
 
-  const ali = (e)=> {
-    e.preventDefault()
-    console.log("ali")
-    alert (`name is ${input}`)
-    setInput("")
-  }
-  const beyda = (e) => {
-    setInput(e.target.value)
+
+  const [degerlendirme, setDegerlendirme] = useState(false)
+  
+  const deg = () => {
+    // setValue("beyda")
+    setDegerlendirme(!degerlendirme)
+    console.log(degerlendirme);
   }
 
   return (
     <div className="App">
-        <form onSubmit={ali}>
-          <label htmlFor="name">Name : </label>
-          <input type="text" name="name" id="name" value= {input} onChange={beyda}/>
-          <button >Sub</button>
-          <span>{input}</span>
-        </form>
-
+    {degerlendirme ? <h1>ali aksoy</h1> : <h1>Beyda</h1> }
+    
+    <button onClick={deg}>değiştir</button>
       
     </div>
   )
