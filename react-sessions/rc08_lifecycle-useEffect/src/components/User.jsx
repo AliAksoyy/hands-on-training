73eefa4fdc16348c433aa035d525f654bbb1cffd
AliAksoyy <alifeyza4017@gmail.com>
2022-10-17 23:19:38 +0300

    added

--- react-sessions/rc08_lifecycle-useEffect/src/components/User.jsx
+++ react-sessions/rc08_lifecycle-useEffect/src/components/User.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react"
 
 
   const User = () => {
+    const [user, setUser] = useState("")
+
+    useEffect(()=> {
+        
+       
+            fetch("https://randomuser.me/api/").then((res)=> res.json())
+            .then((data)=> setUser(data.results[0]))
+          
+        
+        
+    },[])
+    const {name,dob,email,picture} =user
+    console.log(name);
     return(
         <div>
 
-            <h1>name</h1>
-            <img src="" alt="" />
-            <p>email</p>
-            <h5>DOB</h5>
+            <h1>{name?.first} {name?.last}</h1>
+            <img className="rounded-circle" src={picture?.large} alt="" />
+            <p>{email}</p>
+            <h5>{dob?.date}</h5>
+            <button className="btn btn-danger">Get User</button>
         </div>
     )
 }
