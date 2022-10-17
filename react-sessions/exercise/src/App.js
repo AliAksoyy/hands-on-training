7f4b8a903c7e236c622088f750171a4d75c71dab
AliAksoyy <alifeyza4017@gmail.com>
2022-10-17 12:01:25 +0300

    added

--- react-sessions/exercise/src/App.js
+++ react-sessions/exercise/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../src/App.css"
 
 const App = () => {
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  }
+
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      alert ($`{e.target.value}`)
+  }
+ 
+
   return (
     <div className="App">
-      App
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input id="name" name="name" onChange={handleChange} />
+      <button>Submit</button>
+    </form>
+      
     </div>
   )
 }
