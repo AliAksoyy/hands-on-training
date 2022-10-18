b3443e73afc57df0e7b584de14f05834e4de6eb5
AliAksoyy <alifeyza4017@gmail.com>
2022-10-18 20:57:10 +0300

    added

--- react-sessions/rc09_useEffect-axios-example/tutorial-app-inclass/src/pages/Home.jsx
+++ react-sessions/rc09_useEffect-axios-example/tutorial-app-inclass/src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
-
+import axios from "axios"
+import { useState, useEffect } from "react";
 const Home = () => {
+  const [tutorials, setTutorials] =useState([])
+
+    const url = "https://axios-example-cw.herokuapp.com/api/tutorials"
+
+    const getTutorials = async() => {
+      const {data } = await axios(url)
+      console.log(data)
+      setTutorials(data)
+    }
+    useEffect(()=> {
+      getTutorials()
+       
+    },[])
   return (
     <>
       <AddTutorial />
-      <TutorialList />
+      <TutorialList tutorials={tutorials} />
     </>
   );
 };
