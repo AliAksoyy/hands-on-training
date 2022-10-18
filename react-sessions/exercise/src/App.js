b375435a2cf3b90bb06223a217c9959b63373dd0
AliAksoyy <alifeyza4017@gmail.com>
2022-10-18 10:41:32 +0300

    added

--- react-sessions/exercise/src/App.js
+++ react-sessions/exercise/src/App.js
@@ -1,28 +1,6 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [first, setFirst] = useState("")
-  
-  useEffect(()=> {
-    axios("https://randomuser.me/api/?results=10")
-    .then((res )=> {
-      return (
-        console.log( res.data.results)
-        res.data.results.map((item)=> ({
-          name: `${item.name.first}`,
-          image: `${item.picture.thumbnail}`
-          
-        }))
-      )
-    })
-      
-    
-        
-  
-    })
-  },[])
- 
   return (
     <div>App</div>
   )
