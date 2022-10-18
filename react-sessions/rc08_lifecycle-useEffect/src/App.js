dd32a519950459475ee60d722b8128725451e555
AliAksoyy <alifeyza4017@gmail.com>
2022-10-18 07:23:06 +0300

    added

--- react-sessions/rc08_lifecycle-useEffect/src/App.js
+++ react-sessions/rc08_lifecycle-useEffect/src/App.js
@@ -1,18 +1,14 @@
-// import { useState } from "react";
-// import LifeCycleMethods from "./components/LifeCycleMethods";
-// import UseEffectHook from "./components/UseEffectHook";
-import User from "./components/User";
+import { LifeCycle } from "./componentsi/LifeCycle";
+
+
 
 function App() {
-  // const [show, setShow] = useState(false)
+ 
 
   return (
     <div className="container text-center mt-4">
-      {/* <button onClick={()=>setShow(!show)} className="btn btn-dark">{!show ? "Hide" : "Show"}</button>
-      {!show &&  <UseEffectHook />} */}
-      {/* {!show &&  <LifeCycleMethods />} */}
-      <User />
-     
+   
+     <LifeCycle />
     </div>
   );
 }
---
+++ react-sessions/rc08_lifecycle-useEffect/src/componentsi/LifeCycle.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+
+
+export class LifeCycle extends React.Component {
+        constructor(props) {
+            console.log("contrucaf")
+                super(props)
+            this.state = {
+                count: 0
+            }
+        }
+        handleClick = () => {
+            this.setState({
+               count: this.state.count +1
+            }) 
+        }
+        componentDidMount() {
+            console.log("Mounted");
+        }
+
+        componentDidUpdate(prevProps,prevState) {
+            console.log("updated")
+            console.log(prevState.count);
+        }
+        render() {
+            console.log("rendered");
+            return(
+                <div className="container text-center" >
+                    <h1>LifeCycle Methods</h1>
+                    <h2>count = <span>{this.state.count}</span></h2>
+                    <button onClick={this.handleClick} className="btn btn-info">Inc</button>
+                </div>
+            )
+        }
+
+}
