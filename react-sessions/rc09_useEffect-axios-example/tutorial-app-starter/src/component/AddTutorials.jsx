8345495bb6007fd149b25b0ac5e8d621686d460a
AliAksoyy <alifeyza4017@gmail.com>
2022-10-20 09:33:13 +0300

    added

--- react-sessions/rc09_useEffect-axios-example/tutorial-app-starter/src/component/AddTutorials.jsx
+++ react-sessions/rc09_useEffect-axios-example/tutorial-app-starter/src/component/AddTutorials.jsx
@@ -3,8 +3,33 @@
 
 const AddTutorials = () => {
     return(
-        <div>
-            addTutoriasl
+        <div className="text-center  p-4 container">
+            <h1 className="display-5 text-danger">Add Your Tutorial</h1>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">
+                    Title
+                    </label>
+                    <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="description" className="form-label">
+                    Description
+                    </label>
+                    <input
+                    type="text"
+                    className="form-control"
+                    id="description"
+                    />
+                </div>
+                <button type="submit" className="btn btn-danger">
+                    Submit
+                </button>
+            </form>
         </div>
     )
 }
