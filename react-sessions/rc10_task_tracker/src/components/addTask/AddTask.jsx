cda9f44bcd21f8c7fc1bf5a6594eac2ff64630b5
AliAksoyy <alifeyza4017@gmail.com>
2022-10-20 22:18:06 +0300

    added

--- react-sessions/rc10_task_tracker/src/components/addTask/AddTask.jsx
+++ react-sessions/rc10_task_tracker/src/components/addTask/AddTask.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import axios from 'axios'
 
 const AddTask = () => {
+
+  const [task, setTask] = useState("")
+  const [date, setDate] = useState("")
+
+  const handleSubmit =(e)=> {
+    e.preventDefault()
+    const newTask={task,date }
+    console.log(newTask);
+    addNewTask(newTask)
+
+  }
+
+  const addNewTask = async(newTask) => {
+
+  const url="https://63518214dfe45bbd55c21ca7.mockapi.io/api/v1/task"
+    try {
+    await axios.post(url, newTask)
+      
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+
   return (
-    <div>AddTask</div>
+    <div>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="task">
+            <Form.Label>Task</Form.Label>
+            <Form.Control onChange={(e)=> setTask(e.target.value)} type="text" placeholder="Enter text" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="date">
+            <Form.Label>Date</Form.Label>
+            <Form.Control onChange={(e)=> setDate(e.target.value)} type="date" placeholder="Enter Date" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Save
+          </Button>
+        </Form>
+    </div>
   )
 }
 
