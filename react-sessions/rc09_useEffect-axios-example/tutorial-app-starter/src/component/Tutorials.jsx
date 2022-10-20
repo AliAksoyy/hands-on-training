e238dbc9b93af3daeac7576949b3accb98ae3310
AliAksoyy <alifeyza4017@gmail.com>
2022-10-20 09:44:44 +0300

    added

--- react-sessions/rc09_useEffect-axios-example/tutorial-app-starter/src/component/Tutorials.jsx
+++ react-sessions/rc09_useEffect-axios-example/tutorial-app-starter/src/component/Tutorials.jsx
@@ -2,8 +2,33 @@
 
 function Tutorials() {
     return(
-        <div>
-            Tutorials
+        <div className="text-center  p-4 container">
+            <table className="table table-success table-striped">
+                <thead>
+                    <tr>
+                    <th scope="col">#id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+
         </div>
     )
 }
