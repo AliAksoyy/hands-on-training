0778be729c767abc4768260913660ce35646218d
AliAksoyy <alifeyza4017@gmail.com>
2022-10-22 13:48:11 +0300

    added

--- react-sessions/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetails.jsx
+++ react-sessions/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetails.jsx
@@ -8,7 +8,17 @@ const PersonDetails = () => {
     const {state:person} = useLocation()
     console.log(person);
   return (
-    <div>PersonDetails</div>
+    <div className='container text-center'>
+        <h3>{person.first_name} {person.last_name}</h3>
+        <img className='rounded' src={person.avatar} alt="" />
+        <p>{person.email}</p>
+        <div>
+            <button className="btn btn-success me-2">Home</button>
+            <button className="btn btn-warning">Go Back</button>
+        </div>
+
+
+    </div>
   )
 }
 
