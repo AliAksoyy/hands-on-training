cf6b3edd278559aa078741fa70e822cb5fd24f06
AliAksoyy <alifeyza4017@gmail.com>
2022-10-22 09:06:59 +0300

    added

--- react-sessions/exercise/src/App.js
+++ react-sessions/exercise/src/App.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import {Routes, Route, Link, NavLink} from "react-router-dom"
+import Blog from './Pages/Blog'
+import Contact from './Pages/Contact'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+
+    
+    <nav>
+      <NavLink to="/" className={(isActive)=> isActive && "ali"}>Home</NavLink>
+      <NavLink to="/contact" style={({isActive})=> ({
+        color: isActive ? 'red' : 'blue'})}>Contact</NavLink>
+      <NavLink to="/blog">
+      {({isActive})=> (
+        <>Blog {isActive && "akitf"}</>
+      )}
+    </NavLink>
+    </nav>
+
+
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/blog' element={<Blog />} />
+    </Routes>
+    </>
   )
 }
 
