c33b668b1965eb32760ba5acf5a36e945d556b3d
AliAksoyy <alifeyza4017@gmail.com>
2022-10-22 13:44:30 +0300

    added

--- react-sessions/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetails.jsx
+++ react-sessions/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetails.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {useParams, useLocation} from "react-router-dom"
+
 
 const PersonDetails = () => {
+    const {id} =useParams()
+    console.log(id);
+    const {state:person} = useLocation()
+    console.log(person);
   return (
     <div>PersonDetails</div>
   )
