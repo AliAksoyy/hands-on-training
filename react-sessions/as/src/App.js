4a909572e37499cdd51b0358fff8b56949972433
AliAksoyy <alifeyza4017@gmail.com>
2022-10-24 07:05:15 +0300

    added

--- react-sessions/as/src/App.js
+++ react-sessions/as/src/App.js
@@ -4,17 +4,23 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import People from "./pages/People";
 import NotFound from "./pages/NotFound";
+import {Routes, Route} from "react-router-dom"
+import Path from "./pages/Path";
 
 function App() {
   return (
-    <>
+    <div>
       <Nav />
-      <Home />
-      <People />
-      <Contact />
-      <NotFound />
+      <Routes>
+         <Route path="/" element={ < Home /> }/>
+         <Route path="/people" element={ < People /> }/>
+         <Route path="/contact" element={ < Contact /> }/>
+         <Route path="/path" element={ < Path /> }/>
+         <Route path="*" element={ < NotFound /> }/>
+        
+      </Routes>
       <Footer />
-    </>
+    </div>
   );
 }
 
