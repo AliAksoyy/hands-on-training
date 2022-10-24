74cda96e41144b992f896d21270efdeeaf53c6ac
AliAksoyy <alifeyza4017@gmail.com>
2022-10-24 12:18:03 +0300

    added

--- react-sessions/exercise/src/App.js
+++ react-sessions/exercise/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import Home from './pages/Home'
+
+
+import { Link,NavLink, Routes,Route } from 'react-router-dom'
+import Contact from './pages/Contact'
+import Blog from './pages/Blog'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <nav>
+       <Link to="/">Anasayfa</Link>
+       <Link to="/contact">Contact</Link>
+       <Link to="/blog">Blog</Link>
+    </nav>
+
+      <Routes> 
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/blog' element={<Blog/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+    </>
+    
+   
   )
 }
 
