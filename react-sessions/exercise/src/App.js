3eb485945e48353a4db730d628617fa10464b76f
AliAksoyy <alifeyza4017@gmail.com>
2022-10-24 12:02:36 +0300

    added

--- react-sessions/exercise/src/App.js
+++ react-sessions/exercise/src/App.js
@@ -1,29 +1,8 @@
 import React from 'react'
-import {Routes, Route} from "react-router-dom"
-import Home from "./pages/Home" 
-import About from "./pages/About" 
-import Book from "./pages/Book" 
-import BookList from "./pages/BookList" 
-import Contact from "./pages/Contact" 
-import NewBook from "./pages/NewBook" 
-import NotFound from "./pages/NotFound" 
-import Navbar from './component/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/book" element={<Book/>} />
-        <Route path="/booklist" element={<BookList/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/newbokk" element={<NewBook/>} />
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
-
-    </div>
+    <div>App</div>
   )
 }
 
