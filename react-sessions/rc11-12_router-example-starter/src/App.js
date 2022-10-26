76d19afe71fac211df33fc12bc618a8df3096a4d
AliAksoyy <alifeyza4017@gmail.com>
2022-10-26 21:34:05 +0300

    added

--- react-sessions/rc11-12_router-example-starter/src/App.js
+++ react-sessions/rc11-12_router-example-starter/src/App.js
@@ -4,11 +4,19 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import People from "./pages/People";
 import NotFound from "./pages/NotFound";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Nav />
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/instructors" element={<People />} />
+       
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Home />
       <People />
       <Contact />
