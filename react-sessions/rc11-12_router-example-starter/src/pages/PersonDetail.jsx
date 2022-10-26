1cb374e89add49c4480f077aa44f3ebc3eb6c86c
AliAksoyy <alifeyza4017@gmail.com>
2022-10-26 23:15:26 +0300

    added

--- react-sessions/rc11-12_router-example-starter/src/pages/PersonDetail.jsx
+++ react-sessions/rc11-12_router-example-starter/src/pages/PersonDetail.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import {useParams, useLocation} from "react-router-dom"
+import {useParams, useLocation, Navigate, useNavigate} from "react-router-dom"
 
 const PersonDetail = () => {
+    const navigate = useNavigate()
     const {id} = useParams()
     console.log(id);
 
-    const {state:name} = useLocation()
-    console.log(name)
+    const {state:person} = useLocation()
+    console.log(person)
 
   return (
-    <div>PersonDetail</div>
+    <div className='container text-center'>
+    <h3>{person.first_name} {person.last_name}</h3>
+    <img className='rounded' src={person.avatar} alt="" />
+    <p>{person.email}</p>
+    <div>
+        <button className='btn btn-success' onClick={()=> navigate("/")} >Home</button>
+        <button className='btn btn-warning ms-2' onClick={()=> navigate(-1)}>Go Back</button>
+    </div>
+
+
+    </div>
   )
 }
 
