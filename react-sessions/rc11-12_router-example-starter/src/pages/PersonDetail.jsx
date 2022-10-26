839d27ba5e9c77de30352798f5a18041a76648a8
AliAksoyy <alifeyza4017@gmail.com>
2022-10-26 23:33:48 +0300

    added

--- react-sessions/rc11-12_router-example-starter/src/pages/PersonDetail.jsx
+++ react-sessions/rc11-12_router-example-starter/src/pages/PersonDetail.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import {useParams, useLocation, Navigate, useNavigate} from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+import {useParams, useLocation,  useNavigate} from "react-router-dom"
 
 const PersonDetail = () => {
+    const [person, setPeople] = useState("")
     const navigate = useNavigate()
     const {id} = useParams()
     console.log(id);
 
-    const {state:person} = useLocation()
-    console.log(person)
+    // const {state:person} = useLocation()
+    // console.log(person)
+
+  useEffect(()=> {
+        fetch(`https://reqres.in/api/users/${id}`)
+        .then((res) => res.json())
+        .then((data)=> setPeople(data.data))
+        .catch((err)=> console.log(err));
+  },[])
 
   return (
-    <div className='container text-center'>
-    <h3>{person.first_name} {person.last_name}</h3>
-    <img className='rounded' src={person.avatar} alt="" />
-    <p>{person.email}</p>
-    <div>
-        <button className='btn btn-success' onClick={()=> navigate("/")} >Home</button>
-        <button className='btn btn-warning ms-2' onClick={()=> navigate(-1)}>Go Back</button>
-    </div>
-
-
-    </div>
+   
+       
+           
+               
+            <div className='container text-center' key={id}>
+                        <h3>{person.first_name} {person.last_name}</h3>
+                        <img className='rounded' src={person.avatar} alt="" />
+                        <p>{person.email}</p>
+                        <div>
+                            <button className='btn btn-success' onClick={()=> navigate("/")} >Home</button>
+                            <button className='btn btn-warning ms-2' onClick={()=> navigate(-1)}>Go Back</button>
+                        </div>
+            </div>
+                
+       
+    
+    
   )
 }
 
