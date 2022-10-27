3dae08403dfd5bf484b1e60c6d515da8249f6d2b
AliAksoyy <alifeyza4017@gmail.com>
2022-10-27 10:18:51 +0300

    added

--- react-sessions/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
+++ react-sessions/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
@@ -1,29 +1,9 @@
-import { Button, TomatoButton } from "./components/Button.styled";
-import Container from "./components/Container.styled";
-import HeaderText from "./components/HeaderText.styled";
-import StyledLink from "./components/Link.styled";
+import React from 'react'
 
 const App = () => {
   return (
-    <>
-      <Container bg="grey">
-        <HeaderText color="hotpink">STYLED COMPONENTS</HeaderText>
-        <Button primary>Save</Button>
-        <Button>Clear</Button>
-        <TomatoButton>Send</TomatoButton>
-        <TomatoButton primary>Submit</TomatoButton>
-      </Container>
-      <Container bg="#eee">
-        <StyledLink href="https://clarusway.com/" target="_blank">
-          Clarusway
-        </StyledLink>
+    <div>App</div>
+  )
+}
 
-        <StyledLink secondary href="https://reactjs.org/" target="_blank">
-          React
-        </StyledLink>
-      </Container>
-    </>
-  );
-};
-
-export default App;
+export default App
