398c3a96ebbd2b46992c1b5613c928de1fc42cc7
AliAksoyy <alifeyza4017@gmail.com>
2022-10-29 01:05:46 +0300

    added

--- exercise-yasin-abi/src/pages/Details/Details.jsx
+++ exercise-yasin-abi/src/pages/Details/Details.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import {useLocation} from "react-router-dom"
+import { ContentDiv, ContentImage, DetailsDiv, NutrientsDiv, RecipeOrderDiv, TitleDiv, TitleImage } from './Detail.style'
+import pic from "../../assets/diet.svg"
 
 export const Details = () => {
   const {state:recipe} =useLocation()
   console.log(recipe)
   return (
-    <div>Details</div>
+    <DetailsDiv>
+      <TitleDiv>
+        <h1>{recipe.label}</h1>
+        <TitleImage src={pic} />
+      </TitleDiv>
+      <ContentDiv>
+        <NutrientsDiv>
+          <p>as</p>
+        </NutrientsDiv>
+        <ContentImage src={} />
+        <RecipeOrderDiv>
+          <p>sadas</p>
+        </RecipeOrderDiv>
+      </ContentDiv>
+    </DetailsDiv>
   )
 }
 
