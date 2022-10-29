bc40429e19d755382d58437515adb94843168358
AliAksoyy <alifeyza4017@gmail.com>
2022-10-29 16:31:21 +0300

    added

--- exercise-yasin-abi/src/pages/Register/Register.jsx
+++ exercise-yasin-abi/src/pages/Register/Register.jsx
@@ -3,9 +3,6 @@ import { RegisterButton, RegisterDiv } from './Register.style'
 
 export const Register = () => {
   
-  const [registerUsername, setRegisterUsername] = useState("")
-  const [registerEmail, setRegisterEmail] = useState("")
-  const [registerPassword, setRegisterPassword] = useState("")
   const [register, setRegister] = useState({
     registerUsername:"",
     registerEmail:"",
@@ -13,44 +10,45 @@ export const Register = () => {
   });
 
   const handleRegister=(e)=> {
-    e.preventDefault()
-    setRegister({...register, registerUsername, registerEmail,registerPassword })
-    localStorage.setItem(register.registerUsername, JSON.stringify({...register}))
-    setRegisterUsername("")
-    setRegisterEmail("")
-    setRegisterPassword("")
+        e.preventDefault()
+        setRegister({...register,  })
+        console.log(register)
+        localStorage.setItem(new Date().getTime(), JSON.stringify({...register}))
+        setRegister({
+        registerUsername:"",
+        registerEmail:"",
+        registerPassword:""
+      })
+
   }
-console.log(register)
   
   return (
     <RegisterDiv>
       <h2 style={{margin:"1rem 0", fontSize:"2.5rem"}}>Register</h2>
      
-      <form style={{border:"1px solid white", padding:"2rem"}}>
+      <form   style={{border:"1px solid white", padding:"2rem"}}>
             <input type={"text"} 
+            name={"registerUsername"}
             placeholder={"enter your a username"} 
             style={{display:"block",width:"188px",padding:"0.34rem"}}
-            onChange={(e)=> setRegisterUsername(e.target.value)}
-            value={registerUsername}
-            
+            onChange={(e)=> setRegister({...register, [e.target.name]: e.target.value})}
+            value={register.registerUsername}      
             />
             <input type={"email"} 
+            name={"registerEmail"}
             placeholder={"enter your an email"} 
             style={{display:"block",width:"188px",padding:"0.34rem", margin:"1rem 0"}}
-            onChange={(e)=> setRegisterEmail(e.target.value)}
-            value={registerEmail}
-            
-
+            onChange={(e)=> setRegister({...register, [e.target.name]:e.target.value})}
+            value={register.registerEmail}
             />
             <input type={"password"} 
+            name={"registerPassword"}
             placeholder={"enter your a password"}
             style={{display:"block", width:"188px",margin:"1rem 0", padding:"0.34rem"}}
-             onChange={(e)=> setRegisterPassword(e.target.value)}
-             value={registerPassword}
-            
-
+             onChange={(e)=> setRegister({...register, [e.target.name]:e.target.value})}
+             value={register.registerPassword}
             />
-            <RegisterButton onSubmit={(e)=>handleRegister(e)} >Register</RegisterButton>
+            <RegisterButton onClick={handleRegister} >Register</RegisterButton>
           
       </form>
     </RegisterDiv>
