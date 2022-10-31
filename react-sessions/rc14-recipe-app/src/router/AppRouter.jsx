487c0a9e888107f27a4a50f000e56d417e894caf
AliAksoyy <alifeyza4017@gmail.com>
2022-10-31 20:29:32 +0300

    added

--- react-sessions/rc14-recipe-app/src/router/AppRouter.jsx
+++ react-sessions/rc14-recipe-app/src/router/AppRouter.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import { GlobalStyle } from '../style/GlobalStyle'
+import Home from "../pages/home/Home"
+import Login from "../pages/login/Login"
+import Detail from "../pages/detail/Detail"
+import About from "../pages/about/About"
 
 const AppRouter = () => {
   return (
 
     <BrowserRouter>
     <GlobalStyle />
-    <Route>
-      
-    </Route>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/detail" element={<Detail />} />
+    </Routes>
      
     </BrowserRouter>
   )
