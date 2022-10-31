5cf511021bb52fc6202b34deff07911e3110682e
AliAksoyy <alifeyza4017@gmail.com>
2022-10-31 20:48:48 +0300

    added

--- react-sessions/rc14-recipe-app/src/router/AppRouter.jsx
+++ react-sessions/rc14-recipe-app/src/router/AppRouter.jsx
@@ -5,6 +5,7 @@ import Home from "../pages/home/Home"
 import Login from "../pages/login/Login"
 import Detail from "../pages/detail/Detail"
 import About from "../pages/about/About"
+import PrivateRouter from './PrivateRouter'
 
 const AppRouter = () => {
   return (
@@ -14,7 +15,10 @@ const AppRouter = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<PrivateRouter />} >
+        <Route path="" element={<About />} />
+        <Route path="/detail" element={<Detail />} />
+      </Route>
       <Route path="/detail" element={<Detail />} />
     </Routes>
      
