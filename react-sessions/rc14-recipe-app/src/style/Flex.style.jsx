912bb2c3336fb237d2b3734ec36b5a39e4566fc0
AliAksoyy <alifeyza4017@gmail.com>
2022-10-31 20:53:14 +0300

    added

--- react-sessions/rc14-recipe-app/src/style/Flex.style.jsx
+++ react-sessions/rc14-recipe-app/src/style/Flex.style.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components" 
 
 export const Flex = styled.div`
-
+display:flex;
+justify-content:${({justify})=> (justify ? justify : "center")};
+align-items: ${({align})=> (align ? align : "center")}
 `
