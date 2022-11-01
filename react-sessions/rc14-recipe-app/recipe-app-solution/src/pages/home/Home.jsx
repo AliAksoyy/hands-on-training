06bb07cca4bb5d01f3b69bfac6de241db4982c54
AliAksoyy <alifeyza4017@gmail.com>
2022-11-01 20:31:18 +0300

    added

--- react-sessions/rc14-recipe-app/recipe-app-solution/src/pages/home/Home.jsx
+++ react-sessions/rc14-recipe-app/recipe-app-solution/src/pages/home/Home.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 const Home = () => {
+  const [query,, setQuery,] = useState("egg")
+  const [selectedMeal, setSelectedMeal] = useState("breakfast")
+  const [data, setData] = useState("")
+
+  const APP_ID="61dbc14c"
+  const APP_KEY ="0962eed2822938c8ade079f6856cfc98	"
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`
+
+  const getData= async() => {
+    const {data} = await axios(url)
+    setData(data.hits)
+  }
+
+  console.log(data)
+
+  useEffect(()=> {
+    getData()
+  },[])
+
+
   return (
     <div>
       Home
