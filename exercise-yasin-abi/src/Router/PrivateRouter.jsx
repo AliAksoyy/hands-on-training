931561f9fe89d5a0612f46f7960d76019b79e37f
AliAksoyy <alifeyza4017@gmail.com>
2022-11-02 07:40:48 +0300

    added

--- exercise-yasin-abi/src/Router/PrivateRouter.jsx
+++ exercise-yasin-abi/src/Router/PrivateRouter.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Outlet,Navigate } from 'react-router-dom'
+
 
 const PrivateRouter = () => {
+const userAuthentic = true;
   return (
-    <div>PrivateRouter</div>
+    <>
+   {userAuthentic ? <Outlet />  : <Navigate to="/register" /> }
+   </>
   )
 }
 
