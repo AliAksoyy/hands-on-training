a04fe15f175b06c91e6cd86c78023acf6f87fe99
AliAksoyy <alifeyza4017@gmail.com>
2022-11-02 11:51:35 +0300

    added

--- exercise-yasin-abi/src/Router/AppRouter.jsx
+++ exercise-yasin-abi/src/Router/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Home from "../pages/Home/Home"
 import About from "../pages/About/About"
+import Details from "../pages/Details/Details"
 import Login from "../pages//Login/Login"
 import Register from "../pages/Register/Register"
 import PrivateRouter from './PrivateRouter'
@@ -15,7 +16,7 @@ const AppRouter = () => {
       <Route path="/home" element={<Home />} />
       <Route path="/private" element={<PrivateRouter />} >
         <Route path="/private/about" element={<About />} />
-        <Route path="/private/details" element={<About />} />
+        <Route path="/private/details" element={<Details />} />
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Register />} />
