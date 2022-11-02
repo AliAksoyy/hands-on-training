6d90cc79705c095c54d9b093a1e6dafcd3f82991
AliAksoyy <alifeyza4017@gmail.com>
2022-11-02 21:18:55 +0300

    added

--- react-sessions/rc16_context-api/context-example1/context-example1-inClass/src/App.js
+++ react-sessions/rc16_context-api/context-example1/context-example1-inClass/src/App.js
@@ -6,9 +6,11 @@ import People from "./pages/People";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PersonDetail from "./pages/PersonDetail";
 import Login from "./pages/Login";
+import { LoginContext } from "./context/LoginContext";
 
 function App() {
   return (
+    <LoginContext.Provider>
     <BrowserRouter>
       <Navs />
       <Routes>
@@ -23,6 +25,7 @@ function App() {
       </Routes>
       <Footer />
     </BrowserRouter>
+    </LoginContext.Provider>
   );
 }
 
