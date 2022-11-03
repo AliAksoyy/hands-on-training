c9c08a17e08dd7e1bfc787a15744b1afa45ec39b
AliAksoyy <alifeyza4017@gmail.com>
2022-11-03 16:19:31 +0300

    added

--- exercise-yasin-abi/src/components/Navbar/Navbar.jsx
+++ exercise-yasin-abi/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,18 @@
-import { LinksDiv, NavbarDiv, NavbarImg } from "./Navbar.style"
+import { Hambur, LinksDiv, NavbarDiv, NavbarImg } from "./Navbar.style"
 import navbarimg from "../../assets/tastybites.png"
 import { NavLink } from "react-router-dom"
+import {GiHamburgerMenu} from "react-icons/gi"
 
 
 const Navbar = () => {
     return(
         <NavbarDiv>
             <NavbarImg src={navbarimg} />
+            <Hambur >
+            <GiHamburgerMenu />
+
+            </Hambur>
+
             <LinksDiv >
                 <NavLink style={({isActive})=> ({color: isActive ? "red" :"white", textDecoration:"none" }) } to="/home" >Home</NavLink>
                 <NavLink style={({isActive})=> ({color:isActive ? "red": "white", textDecoration:"none"})} to="/private/about">About</NavLink>
--- exercise-yasin-abi/src/components/Navbar/Navbar.style.jsx
+++ exercise-yasin-abi/src/components/Navbar/Navbar.style.jsx
@@ -6,7 +6,9 @@ export const NavbarDiv = styled.div`
     align-items:center;
     justify-content:space-between;
     height:10vh;
-    background-color: ${({theme})=> theme.colors.nav}
+    background-color: ${({theme})=> theme.colors.nav};
+ 
+
 `
 export const NavbarImg = styled.img`
     width:60px;
@@ -18,5 +20,23 @@ export const LinksDiv = styled.div`
     justify-content:space-evenly;
     width:60%;
     gap:1rem;
+    @media screen and (max-width:${({theme})=>theme.responsive.lg }) {
+           flex-direction:column;
+           border:2px solid black;
+        
+           
+        }
 
+`
+export const Hambur =styled.div`
+       display:none;
+       cursor:pointer;
+       color:crimson;
+        @media screen and (max-width:${({theme})=>theme.responsive.lg }) {
+            display:block;
+           border:2px solid black;
+
+           
+        }
+       
 `
