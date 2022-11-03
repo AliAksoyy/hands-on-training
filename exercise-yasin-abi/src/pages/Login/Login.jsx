c669b8ca58c102a5147f2f7c5227f52917485f51
AliAksoyy <alifeyza4017@gmail.com>
2022-11-03 11:07:17 +0300

    added

--- exercise-yasin-abi/src/pages/Login/Login.jsx
+++ exercise-yasin-abi/src/pages/Login/Login.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { AliDiv, LoginForm, LoginDiv } from './Login.style'
+import beyda from "../../assets/meal.svg"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <LoginDiv>
+    <AliDiv>
+      <img style={{width:"100px"}} src={beyda} alt="d" />
+      <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+      <LoginForm>
+        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="text" placeholder='username'/>
+        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="password" placeholder='pasword' />
+        <button style={{width:"10rem"}}>LOGIN</button>
+      </LoginForm>
+
+    </AliDiv>
+    </LoginDiv>
   )
 }
 
