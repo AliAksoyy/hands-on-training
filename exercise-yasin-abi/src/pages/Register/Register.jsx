0e7e686fd0e980dc2c8035d6ebd3523fbbf421dd
AliAksoyy <alifeyza4017@gmail.com>
2022-11-03 11:10:31 +0300

    added

--- exercise-yasin-abi/src/pages/Register/Register.jsx
+++ exercise-yasin-abi/src/pages/Register/Register.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { AliDiv, LoginForm, LoginDiv } from './Register.style'
 
-const Register = () => {
+
+const Login = () => {
   return (
-    <div>Register</div>
+    <LoginDiv>
+    <AliDiv>
+      
+      <LoginForm>
+        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="text" placeholder='username'/>
+        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="email" placeholder='email'/>
+        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="password" placeholder='pasword' />
+        <button style={{width:"10rem"}}>LOGIN</button>
+      </LoginForm>
+
+    </AliDiv>
+    </LoginDiv>
   )
 }
 
-export default Register
+export default Login
---
+++ exercise-yasin-abi/src/pages/Register/Register.style.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const LoginDiv=styled.div`
+background-color: #a21;
+height:90vh;
+display:flex;
+align-items:center;
+justify-content:center;
+
+`
+
+export const AliDiv=styled.div`
+background-color:rgba(0,12,232,0.3);
+height:22rem;
+width:22rem;
+border-radius:50%;
+justify-content:center;
+display:flex;
+flex-direction:column;
+align-items:center;
+text-align:center;
+gap:2rem;
+padding-top:2rem;
+
+
+`
+
+export const LoginForm = styled.form`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    gap:0.5rem;
+
+`
