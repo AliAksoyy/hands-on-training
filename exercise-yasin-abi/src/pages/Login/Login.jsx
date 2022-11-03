c80fe84ccc2e3886bcabc35d26d0f1f412fb710a
AliAksoyy <alifeyza4017@gmail.com>
2022-11-03 14:05:22 +0300

    added

--- exercise-yasin-abi/src/pages/Login/Login.jsx
+++ exercise-yasin-abi/src/pages/Login/Login.jsx
@@ -1,17 +1,52 @@
 import React from 'react'
 import { AliDiv, LoginForm, LoginDiv } from './Login.style'
 import beyda from "../../assets/meal.svg"
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+  const navigate =useNavigate()
+
+  const [login,setLogin] =useState({
+    username:"",
+    password:""
+  });
+
+const handleSubmit =(e) => {
+  e.preventDefault()
+  const localvalue =Object.values(localStorage).map((item)=> JSON.parse(item))
+
+  if(localvalue.some((item)=> item.username === login.username || localvalue.some((item)=> item.password === login.password))) {
+    document.querySelector(".loginshow").innerText = `onaylandınız `
+    navigate("/home")
+
+  }else {
+    document.querySelector(".loginshow").innerText = `bilgiler hatalı tekrar register olunuz `
+    setTimeout(() => {
+      navigate("/")
+    }, 2000);
+  }
+
+  setLogin({
+    username:"",
+    password:""
+  });
+
+}
+
+
+
+
   return (
     <LoginDiv>
     <AliDiv>
       <img style={{width:"100px"}} src={beyda} alt="d" />
-      <h5>Lorem ipsum dolor sit amet consectetur.</h5>
-      <LoginForm>
-        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="text" placeholder='username'/>
-        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="password" placeholder='pasword' />
-        <button style={{width:"10rem"}}>LOGIN</button>
+      <h5 className='loginshow'>Lorem ipsum dolor sit amet consectetur.</h5>
+      <LoginForm onSubmit={handleSubmit}>
+        <input onChange={(e)=> setLogin({...login, username:e.target.value}) }  value={login.username} style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="text" placeholder='username'/>
+        <input  onChange={(e)=> setLogin({...login, password:e.target.value}) }  value={login.email} style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="password" placeholder='pasword' />
+        <button type='submit' style={{width:"10rem"}}>LOGIN</button>
       </LoginForm>
 
     </AliDiv>
--- exercise-yasin-abi/src/pages/Register/Register.jsx
+++ exercise-yasin-abi/src/pages/Register/Register.jsx
@@ -1,17 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { AliDiv, LoginForm, LoginDiv } from './Register.style'
 
+let counter=0
+const Register = () => {
+  const navigate = useNavigate()
 
-const Login = () => {
+const[register,setRegister] = useState({
+  username:"",
+  email:"",
+  password:""
+});
+
+const handleinput = (e) => {
+  e.target.type ==="text" && setRegister({...register, username:e.target.value})
+}
+const handleSubmitRegister = (e) => {
+  e.preventDefault()
+  const localvalue = (Object.values(localStorage)).map((item)=> JSON.parse(item))
+ 
+  
+  if(!localvalue.some((item)=> item.username === register.username) || !localvalue.some((item)=> item.email === register.email) || !localvalue.some((item)=> item.password === register.password)) {
+  localStorage.setItem(new Date().getTime(), JSON.stringify(register))
+    document.querySelector(".show").innerText = `başarılı kayıt oldunuz tebrikler yönlediriliyorsunuz`
+    setTimeout(()=> {
+      navigate("/login")
+    },2000)
+   
+  }else {
+    document.querySelector(".show").innerText = `yanlış giriş yaptınız hakkınız ${2-counter} kaldı`
+    counter ++
+    if(counter ===3) {
+      counter = 0
+      document.querySelector("button").disabled=true;
+      setTimeout(() => {
+        document.querySelector("button").disabled=false;
+        document.querySelector(".show").innerText = ""
+      }, 2000);
+
+      
+      
+    }
+  
+}
+  setRegister({
+    username:"",
+    email:"",
+    password:""
+  });
+
+}
+
+
+
+
   return (
     <LoginDiv>
     <AliDiv>
       
-      <LoginForm>
-        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="text" placeholder='username'/>
-        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="email" placeholder='email'/>
-        <input style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="password" placeholder='pasword' />
-        <button style={{width:"10rem"}}>LOGIN</button>
+      <LoginForm onSubmit={handleSubmitRegister}>
+      <p className='show'></p>
+        <input onChange={handleinput} style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="text" placeholder='username' value={register.username}/>
+        <input onChange={(e)=> setRegister({...register, email:e.target.value})}  value={register.email} style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="email" placeholder='email'/>
+        <input onChange={(e)=> setRegister({...register, password:e.target.value})}  value={register.password} style={{backgroundColor:"rgba(0,0,0,0.7)", outline:"none"}} type="password" placeholder='pasword' />
+        <button type='submit' style={{width:"10rem"}}>LOGIN</button>
       </LoginForm>
 
     </AliDiv>
@@ -19,4 +71,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Register
