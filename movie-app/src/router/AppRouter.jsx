c2266c325915dc7d109b6ccb4ec1f91fedbdb6f8
AliAksoyy <alifeyza4017@gmail.com>
2022-11-06 16:23:43 +0300

    added

--- movie-app/src/router/AppRouter.jsx
+++ movie-app/src/router/AppRouter.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {Routes,Route} from "react-router-dom"
+import Navbar from "../components/Navbar"
+import Main from "../pages/Main"
+import Login from "../pages/Login"
+import Register from "../pages/Register"
+import PrivateRouter from "./PrivateRouter"
+import MovieDetail from "../pages/MovieDetail"
+
+
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+      <>
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="moviedetail" element={<PrivateRouter />} >
+        <Route path="/" element={<MovieDetail />} /> 
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Main />} />
+        </Routes>
+      </>
   )
 }
 
