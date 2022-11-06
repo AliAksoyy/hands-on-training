9fc24b4aa687495b8caace3db841a18b8b669f11
AliAksoyy <alifeyza4017@gmail.com>
2022-11-06 07:49:50 +0300

    added

--- material-intro-starter/src/components/TextFieldCom.jsx
+++ material-intro-starter/src/components/TextFieldCom.jsx
@@ -2,14 +2,16 @@ import Typography from "@mui/material/Typography"
 import Container from "@mui/material/Container"
 import  Button  from "@mui/material/Button";
 import  Box  from "@mui/material/Box";
-
-import React from 'react'
+import  TextField  from "@mui/material/TextField";
 
 const TextFieldCom = () => {
   return (
    <>
     <Container>
         <Typography variant="h4" color="error" mt={4} align="center">Text Field</Typography>
+        <Box>
+            <TextField />
+        </Box>
     </Container>
    </>
   )
