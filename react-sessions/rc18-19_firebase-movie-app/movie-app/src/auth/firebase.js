f6ae747d6005953a14953b331a57e8b08fb7503f
AliAksoyy <alifeyza4017@gmail.com>
2022-11-09 08:48:38 +0300

    added

--- react-sessions/rc18-19_firebase-movie-app/movie-app/src/auth/firebase.js
+++ react-sessions/rc18-19_firebase-movie-app/movie-app/src/auth/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 //* Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +14,19 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+export const auth = getAuth(app);
+
+export const createUser =async(email,password)=> {
+try {
+
+let userCredential =await createUserWithEmailAndPassword(auth, email, password)
+console.log(userCredential)
+} catch (error) {
+  console.log(error.message);
+}
+}
+
+
+
+
+
