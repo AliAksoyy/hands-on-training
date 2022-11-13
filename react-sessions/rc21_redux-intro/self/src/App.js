11741295dd7b5d43906d5b381b4ff971773c19fa
AliAksoyy <alifeyza4017@gmail.com>
2022-11-13 20:12:11 +0300

    added

--- react-sessions/rc21_redux-intro/self/src/App.js
+++ react-sessions/rc21_redux-intro/self/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import Counter from "./components/counter/Counter";
-import Todo from "./components/todo/Todo";
+// import Todo from "./components/todo/Todo";
+import { legacy_createStore } from "redux";
+import reducer from "./redux";
+import { Provider } from "react-redux";
 
 function App() {
+
+const store = legacy_createStore(reducer)
+
+
+
   return (
     <div className="app">
+    <Provider store={store}>
       <Counter />
       {/* <Todo /> */}
+      </Provider>
     </div>
   );
 }
