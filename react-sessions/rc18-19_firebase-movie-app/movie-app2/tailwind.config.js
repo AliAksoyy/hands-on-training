8a5338aa0fe2fc45b805110cdc9f23abe8f5d9e0
AliAksoyy <alifeyza4017@gmail.com>
2022-11-17 22:59:21 +0300

    added

--- react-sessions/rc18-19_firebase-movie-app/movie-app2/tailwind.config.js
+++ react-sessions/rc18-19_firebase-movie-app/movie-app2/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{html,js,jsx}', './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {},
   },
