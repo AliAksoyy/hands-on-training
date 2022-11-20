18e407ebc07210c1c77ae45db5deb32fd5e3f960
AliAksoyy <alifeyza4017@gmail.com>
2022-11-20 07:53:33 +0300

    added

--- react-sessions/rc24_formik-yup/self/src/pages/Login.jsx
+++ react-sessions/rc24_formik-yup/self/src/pages/Login.jsx
@@ -3,15 +3,24 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/result.svg";
 import { Link, useNavigate } from "react-router-dom";
-
+import {Formik, Form, } from "formik"
 import { useSelector } from "react-redux";
+import { LoadingButton } from "@mui/lab";
+import * as yup from "yup"
+
+
+const loginSchema=yup.object().shape({
+  email:yup.string().email("email giriniz").required("gerekli"),
+  password:yup.string().required("ula").min(8, "8 karakter").max(16, "kardeş yavaş").matches(/[a-zA-Z]+/, "qqq")
+})
 
 const Login = () => {
   const navigate = useNavigate();
-  const { currentUser, error } = useSelector((state) => state?.auth);
+  const { currentUser, error, loading } = useSelector((state) => state?.auth);
 
   return (
     <Container maxWidth="lg">
@@ -49,6 +58,57 @@ const Login = () => {
           >
             Login
           </Typography>
+
+          <Formik
+          initialValues={{email:"", password:""}}
+          validationSchema={loginSchema}
+          onSubmit={(values,actions)=> {
+            //!login(values)
+            actions.resetForm()
+            actions.setSubmitting(false)
+          }}
+          >
+          {({values,errors,handleChange,handleBlur,touched})=> (
+            <Form>
+              <Box sx={{display:"flex", flexDirection:"column", gap:2 }}>
+                <TextField
+                label="Email"
+                id="email"
+                type="email"
+                name="email"
+                value={values?.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.email && Boolean(errors.email)}
+                helperText={touched.email && errors.email }
+                 />
+                <TextField
+                label="Paasword"
+                id="password"
+                type="password"
+                name="password"
+                value={values?.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.password && Boolean(errors.password)}
+                helperText={touched.password && errors.password }
+                 />
+                <LoadingButton 
+                type="submit"
+                loading={loading} 
+                loadingPosition="center"
+                variant="outlined" >
+                Submit
+                </LoadingButton>
+              </Box>
+            </Form>
+          )}
+
+
+          </Formik>
+
+
+
 
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/register">Do you have not an account?</Link>
