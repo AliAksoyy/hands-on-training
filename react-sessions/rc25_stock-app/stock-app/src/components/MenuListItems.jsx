cba20b05b07d302e990a8bcb97186f112b9d47ad
AliAksoyy <alifeyza4017@gmail.com>
2022-11-21 22:34:15 +0300

    added

--- react-sessions/rc25_stock-app/stock-app/src/components/MenuListItems.jsx
+++ react-sessions/rc25_stock-app/stock-app/src/components/MenuListItems.jsx
@@ -10,84 +10,78 @@ import StoreIcon from "@mui/icons-material/Store";
 import StarsIcon from "@mui/icons-material/Stars";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const icons = [
-    {
-      icon: <DashboardIcon />,
-      title: "Dashboard",
-      url: "/stock/",
-    },
-    {
-      title: "Purchase",
-      icon: <ShoppingCartIcon />,
-      url: "/stock/purchases/",
-    },
-    {
-      title: "Sales",
-      icon: <AttachMoneyIcon />,
-      url: "/stock/sales/",
-    },
-    {
-      title: "Firms",
-      icon: <StoreIcon />,
-      url: "/stock/firms/",
-    },
-    {
-      title: "Brands",
-      icon: <StarsIcon />,
-      url: "/stock/brands/",
-    },
-    {
-      title: "Products",
-      icon: <InventoryIcon />,
-      url: "/stock/products/",
-    },
-    {
-        title:"Admin Panel",
-        icon : <SupervisorAccountIcon />,
-        url : "https://14281.fullstack.clarusway.com/admin"
-    }
-  ];
+  {
+    icon: <DashboardIcon />,
+    title: "Dashboard",
+    url: "/stock/",
+  },
+  {
+    title: "Purchase",
+    icon: <ShoppingCartIcon />,
+    url: "/stock/purchases/",
+  },
+  {
+    title: "Sales",
+    icon: <AttachMoneyIcon />,
+    url: "/stock/sales/",
+  },
+  {
+    title: "Firms",
+    icon: <StoreIcon />,
+    url: "/stock/firms/",
+  },
+  {
+    title: "Brands",
+    icon: <StarsIcon />,
+    url: "/stock/brands/",
+  },
+  {
+    title: "Products",
+    icon: <InventoryIcon />,
+    url: "/stock/products/",
+  },
+  {
+    title: "Admin Panel",
+    icon: <SupervisorAccountIcon />,
+    url: "https://10001.fullstack.clarusway.com/admin",
+  },
+];
 
 const iconStyle = {
-    color:"#eee",
-    "& .MuiSvgIcon-root" : {color:"#eee"},
-    "&:hover": {color:"red"},   
-
-}
-
+  color: "#eee",
+  "& .MuiSvgIcon-root": { color: "#eee" },
+  "&:hover": { color: "red" },
+  "&:hover .MuiSvgIcon-root": { color: "red" },
+};
 
 const MenuListItems = () => {
-
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div>
-         <List>
+      <List>
         {icons?.map((item, index) => (
           <ListItem key={index} disablePadding>
-            {!item.url.includes("http") && 
-            ( <ListItemButton sx={iconStyle} onClick={()=> navigate(item.url)}>
-              <ListItemIcon>
-                {item.icon}
-              </ListItemIcon>
-              <ListItemText primary={item.title} />
-            </ListItemButton> 
+            {item.url.includes("http") && (
+              <ListItemButton to={item.url} sx={iconStyle}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
             )}
-            {item.url.includes("http") && 
-            ( <ListItemButton sx={iconStyle} to={item.url}>
-              <ListItemIcon>
-                {item.icon}
-              </ListItemIcon>
-              <ListItemText primary={item.title} />
-            </ListItemButton> 
+
+            {!item.url.includes("http") && (
+              <ListItemButton onClick={() => navigate(item.url)} sx={iconStyle}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
             )}
-            
           </ListItem>
         ))}
       </List>
     </div>
-  )
-}
+  );
+};
 
-export default MenuListItems
+export default MenuListItems;
