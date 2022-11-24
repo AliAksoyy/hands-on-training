cd2e18a7d78ce106d521862c3aa482fbff0ef34a
AliAksoyy <alifeyza4017@gmail.com>
2022-11-24 06:36:35 +0300

    added

--- react-sessions/rc25_stock-app/stock-app/src/app/store.jsx
+++ react-sessions/rc25_stock-app/stock-app/src/app/store.jsx
@@ -1,40 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-import stockReducer from "../features/stockSlice";
-import storage from "redux-persist/lib/storage/"; // defaults to localStorage for web
-
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: persistedReducer,
-    stock: stockReducer,
+    auth: authReducer,
   },
-
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
   devTools: process.env.NODE_ENV !== "production",
 });
-
-export const persistor = persistStore(store);
 export default store;
