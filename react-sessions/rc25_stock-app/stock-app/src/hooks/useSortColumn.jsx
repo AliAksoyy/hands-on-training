e0d53bba9bb9659595b8a0e7eeae69fdf8ef5c94
AliAksoyy <alifeyza4017@gmail.com>
2022-11-24 20:54:00 +0300

    added

---
+++ react-sessions/rc25_stock-app/stock-app/src/hooks/useSortColumn.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const useSortColumn = (data) => {
+  const [sortedProducts, setSortedProducts] = useState(data);
+
+
+    
+
+
+    const handleSort = (arg, type) => {
+        setToggle({ ...toggle, [arg]: toggle[arg] * -1 });
+        setSortedProducts(
+          sortedProducts
+            ?.map((item) => item)
+            .sort((a, b) => {
+              if (type === "date") {
+                return toggle[arg] * (new Date(a[arg]) - new Date(b[arg]));
+              } else if (type === "number") {
+                return toggle[arg] * (a[arg] - b[arg]);
+              } else {
+                if (toggle[arg] === 1) {
+                  return b[arg] > a[arg] ? 1 : b[arg] < a[arg] ? -1 : 0;
+                } else {
+                  return a[arg] > b[arg] ? 1 : a[arg] < b[arg] ? -1 : 0;
+                }
+              }
+            })
+        );
+      };
+
+
+
+
+
+
+
+
+
+
+
+  return {}
+}
+
+export default useSortColumn
--- react-sessions/rc25_stock-app/stock-app/src/pages/Products.jsx
+++ react-sessions/rc25_stock-app/stock-app/src/pages/Products.jsx
@@ -30,7 +30,6 @@ const Products = () => {
   }, []);
 
   //? Siralanacak local state (sutun verilerinin local state hali)
-  const [sortedProducts, setSortedProducts] = useState(products);
 
   //! product state'i her guncellendiginde local state'i de guncelle
   useEffect(() => {
@@ -44,26 +43,7 @@ const Products = () => {
   });
 
   //? Jenerik Sutun siralama fonksiyonu
-  const handleSort = (arg, type) => {
-    setToggle({ ...toggle, [arg]: toggle[arg] * -1 });
-    setSortedProducts(
-      sortedProducts
-        ?.map((item) => item)
-        .sort((a, b) => {
-          if (type === "date") {
-            return toggle[arg] * (new Date(a[arg]) - new Date(b[arg]));
-          } else if (type === "number") {
-            return toggle[arg] * (a[arg] - b[arg]);
-          } else {
-            if (toggle[arg] === 1) {
-              return b[arg] > a[arg] ? 1 : b[arg] < a[arg] ? -1 : 0;
-            } else {
-              return a[arg] > b[arg] ? 1 : a[arg] < b[arg] ? -1 : 0;
-            }
-          }
-        })
-    );
-  };
+
 
   return (
     <Box>
