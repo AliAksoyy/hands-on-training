29a05a4419b663cee48c0495f95fdade93a7f58b
AliAksoyy <alifeyza4017@gmail.com>
2022-11-24 10:41:11 +0300

    added

--- react-sessions/rc25_stock-app/stock-app/src/pages/Firms.jsx
+++ react-sessions/rc25_stock-app/stock-app/src/pages/Firms.jsx
@@ -1,5 +1,37 @@
+import axios from "axios"
+import { useSelector, useDispatch } from 'react-redux';
+import {getSuccess} from "../features/stockSlice"
+import {useEffect} from "react"
+
 const Firms = () => {
-  return <div>Firms</div>;
+
+  const {token} = useSelector(state => state.auth)
+  const dispatch=useDispatch()
+ 
+
+  const getData = async() => {
+
+  const BASE_URL = "https://14281.fullstack.clarusway.com";
+    try {
+        const {data} = await axios.get(`${BASE_URL}/stock/firms/`, 
+        {headers: {Authorization: `Token ${token}`}} )
+        console.log(data)
+        dispatch(getSuccess(data, "firm"))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+useEffect(() => {
+ getData()
+}, [])
+
+
+
+
+  return <div>
+  
+  Firms</div>;
 };
 
 export default Firms;
