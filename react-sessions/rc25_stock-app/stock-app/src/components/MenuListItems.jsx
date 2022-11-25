5a269c908de0044bb6510d442979b88da4c408df
AliAksoyy <alifeyza4017@gmail.com>
2022-11-25 07:22:49 +0300

    added

--- react-sessions/rc25_stock-app/stock-app/src/components/MenuListItems.jsx
+++ react-sessions/rc25_stock-app/stock-app/src/components/MenuListItems.jsx
@@ -12,76 +12,81 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
 import { useNavigate } from "react-router-dom";
 
-const icons = [
-  {
-    icon: <DashboardIcon />,
-    title: "Dashboard",
-    url: "/stock/",
-  },
-  {
-    title: "Purchase",
-    icon: <ShoppingCartIcon />,
-    url: "/stock/purchases/",
-  },
-  {
-    title: "Sales",
-    icon: <AttachMoneyIcon />,
-    url: "/stock/sales/",
-  },
-  {
-    title: "Firms",
-    icon: <StoreIcon />,
-    url: "/stock/firms/",
-  },
-  {
-    title: "Brands",
-    icon: <StarsIcon />,
-    url: "/stock/brands/",
-  },
-  {
-    title: "Products",
-    icon: <InventoryIcon />,
-    url: "/stock/products/",
-  },
-  {
-    title: "Admin Panel",
-    icon: <SupervisorAccountIcon />,
-    url: "https://10001.fullstack.clarusway.com/admin",
-  },
-];
 
-const iconStyle = {
-  color: "#eee",
-  "& .MuiSvgIcon-root": { color: "#eee" },
-  "&:hover": { color: "red" },
-  "&:hover .MuiSvgIcon-root": { color: "red" },
-};
+const icons = [
+    {
+      icon: <DashboardIcon />,
+      title: "Dashboard",
+      url: "/stock/",
+    },
+    {
+      title: "Purchase",
+      icon: <ShoppingCartIcon />,
+      url: "/stock/purchases/",
+    },
+    {
+      title: "Sales",
+      icon: <AttachMoneyIcon />,
+      url: "/stock/sales/",
+    },
+    {
+      title: "Firms",
+      icon: <StoreIcon />,
+      url: "/stock/firms/",
+    },
+    {
+      title: "Brands",
+      icon: <StarsIcon />,
+      url: "/stock/brands/",
+    },
+    {
+      title: "Products",
+      icon: <InventoryIcon />,
+      url: "/stock/products/",
+    },
+    {
+      title: "Admin Panel",
+      icon: <SupervisorAccountIcon />,
+      url: "https://10001.fullstack.clarusway.com/admin",
+    },
+  ];
+
+  const iconStyle = {
+    color:"#eee",
+    "& .MuiSvgIcon-root" : {color:"#eee"},
+    "&:hover" : {color:"red"},
+    "&:hover .MuiSvgIcon-root" : {color:"red"}
+  }
+
 
 const MenuListItems = () => {
-  const navigate = useNavigate();
+    const navigate=useNavigate()
   return (
     <div>
-      <List>
-        {icons?.map((item, index) => (
+        <List>
+        {icons.map((icon, index) => (
           <ListItem key={index} disablePadding>
-            {item.url.includes("http") && (
-              <ListItemButton to={item.url} sx={iconStyle}> 
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.title} />
-              </ListItemButton>
-            )}
-
-            {!item.url.includes("http") && (
-              <ListItemButton onClick={() => navigate(item.url)} sx={iconStyle}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.title} />
-              </ListItemButton>
-            )}
+          {icon?.url.includes("http") && (
+            <ListItemButton to={`${icon?.url}`} sx={iconStyle}>
+              <ListItemIcon>
+               {icon.icon}
+              </ListItemIcon>
+              <ListItemText primary={icon.title} />
+            </ListItemButton>
+          )}
+           {!icon?.url.includes("http") && (
+            <ListItemButton onClick={()=> navigate(`${icon?.url}`)} sx={iconStyle}>
+              <ListItemIcon>
+               {icon.icon}
+              </ListItemIcon>
+              <ListItemText primary={icon.title} />
+            </ListItemButton>
+           )}
           </ListItem>
         ))}
-      </List>
+        </List>
     </div>
-  );
-};
+  )
+}
 
-export default MenuListItems;
+export default MenuListItems
--- react-sessions/rc25_stock-app/stock-app/src/features/stockSlice.jsx
+++ react-sessions/rc25_stock-app/stock-app/src/features/stockSlice.jsx
@@ -4,24 +4,26 @@ const stockSlice = createSlice({
   name: "stock",
 
   initialState: {
-    purchases: null,
-    sales: null,
-    products: null,
-    brands: null,
-    firms: null,
-    categories: null,
-    loading: false,
-    error: false,
+    firms:null,
+    brands:null,
+    products:null,
+    purchases:null,
+    sales:null,
+    loading:false,
+    error:false,
+    categories:null
   },
   reducers: {
     fetchStart: (state) => {
       state.loading = true;
       state.error = false;
     },
-    getSuccess: (state, { payload: { data, url } }) => {
-      state.loading = false;
-      state[url] = data;
+
+    getSuccess:(state, {payload:{data,url}})=> {
+      state.loading=false;
+      state[url]= data
     },
+   
     fetchFail: (state) => {
       state.loading = false;
       state.error = true;
@@ -29,5 +31,9 @@ const stockSlice = createSlice({
   },
 });
 
-export const { fetchStart, getSuccess, fetchFail } = stockSlice.actions;
+export const {
+  fetchStart,
+  getSuccess,
+  fetchFail,
+} = stockSlice.actions;
 export default stockSlice.reducer;
