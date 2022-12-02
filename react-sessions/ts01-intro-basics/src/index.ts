031b72ebb3e1f1bc5f620c59cc776ffffe5ae2d4
AliAksoyy <alifeyza4017@gmail.com>
2022-12-02 07:10:09 +0300

    added

--- react-sessions/ts01-intro-basics/src/index.ts
+++ react-sessions/ts01-intro-basics/src/index.ts
@@ -10,4 +10,11 @@ let arr:number[] = []; //[1,2,3]
 arr.push(3)
 console.log(arr);
 
+let b1:unknown =5
+
+let b2:number =b1 as number
+
+let fruits:string[] = ["1","2"]
+let number:Array<number> =[1,2,3]
+
 
