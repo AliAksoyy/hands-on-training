2e01e5b43711f4fa56ccd5d31cc0ff24472e4283
AliAksoyy <alifeyza4017@gmail.com>
2022-12-21 10:54:45 +0300

    aadded

--- code-challenge/codewars.js
+++ code-challenge/codewars.js
@@ -193,37 +193,4 @@
 
 
 
-function convert(num){
-    const obj = {
-        M:1000,
-        CM:900,
-        D:500,
-        CD:400,
-        C:100,
-        XC:90,
-        L:50,
-        XL:40,
-        X:10,
-        IX:9,
-        V:5,
-        IV:4,
-        I:1
-    }
-    let result =""
-    for(let key in obj){
-        console.log("key",  key)
-        console.log("value",  obj[key])
-        while(num>=obj[key]){
-            result +=key;
-            num-=obj[key]
-          
-        }
-    }
-    return result
-
-
-
-}
-
-console.log(convert(18));
 
