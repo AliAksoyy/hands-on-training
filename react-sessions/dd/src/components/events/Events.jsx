8edb49f83c41af751616aff7a027164c2a40b6e1
AliAksoyy <alifeyza4017@gmail.com>
2022-12-29 23:36:06 +0300

    added sytenc event

--- react-sessions/dd/src/components/events/Events.jsx
+++ react-sessions/dd/src/components/events/Events.jsx
@@ -1,6 +1,7 @@
 
 
 const Events = ()=> {
+    let msg="ali aksoy"
 
     const handleClick=(e)=> {
     
@@ -8,11 +9,16 @@ const Events = ()=> {
     const handleClear=(msg)=> {
         alert(msg)
     }
+    const handleChange=(e)=> {
+        e.target.parentElement.firstElementChild.innerText="beyda aksoy"
+    }
 
     return(
         <div id="id" className="container text-center mt-4">
+            <h1>{msg}</h1>
            <button className="btn btn-success" onClick={handleClick} >Click</button> 
            <button onClick={()=>handleClear("clear btn clicked")} className="btn btn-dark">Clear</button>
+           <button onClick={handleChange} className="btn btn-danger">Change</button>
         </div>
     )
 }
