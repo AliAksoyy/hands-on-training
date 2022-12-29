d9c8858d685e847ed9e48026f194be6acb15c14f
AliAksoyy <alifeyza4017@gmail.com>
2022-12-29 13:09:29 +0300

    added

--- react-sessions/dd/src/App.js
+++ react-sessions/dd/src/App.js
@@ -1,18 +1,11 @@
-import Card from "./components/Card";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import data from "./data"
-import "./sass/App.scss"
 
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Card  data={data}/>
-      <Footer />
-    </>
-  );
+const App = () => {
+  return(
+    <div className="App">
+      s
+    </div>
+  )
 }
 
 export default App;
--- react-sessions/dd/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import FooterStyle from "../sass/footer.module.scss"
-const Footer = () => {
-  return (
-    <div className={FooterStyle["footer"]}>Copy right by clarusway {new Date().toDateString()}</div>
-  )
-}
-
-export default Footer
