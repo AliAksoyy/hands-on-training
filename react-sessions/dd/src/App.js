cfda6721a9cb4bf5452452b4dae4459257bf6e16
AliAksoyy <alifeyza4017@gmail.com>
2022-12-29 13:26:11 +0300

    added

--- react-sessions/dd/src/App.js
+++ react-sessions/dd/src/App.js
@@ -1,9 +1,10 @@
-
+import "./App.scss"
 
 const App = () => {
   return(
     <div className="App">
-      s
+      <h1>dsfdsfds</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime eveniet vitae voluptate facere laboriosam modi totam repudiandae. Eveniet fuga repudiandae facere officia perspiciatis excepturi est error dolore laboriosam maiores nemo, ratione aperiam in neque non delectus nesciunt? Nisi inventore doloribus officia consequuntur illo, voluptates eum quia. Nisi consequatur sapiente modi.</p>
     </div>
   )
 }
