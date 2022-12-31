9953006158291428973d06ffb34d2d6834d8a5de
AliAksoyy <alifeyza4017@gmail.com>
2022-12-31 20:10:32 +0300

    new ad

--- react-sessions/dd/src/components/Addtutorials.jsx
+++ react-sessions/dd/src/components/Addtutorials.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Addtutorials = () => {
+
+    const [info,setInfo]=useState({})
+
+const handleChange = (e)=> {
+    setInfo({...info, [e.target.id]:e.target.value})
+}
+
+const handleSubmit = (e)=> {
+   e.preventDefault()
+    console.log(info);
+    setInfo({})
+}
+
   return (
     <div className='container mt-4 p-3 text-center'>
         <h1 className='dispaly-4 text-danger'>Add Your Tutorials</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="title">Title</label>
             <input
             type="text"
             id='title'
             name='title'
             className='form-control'
+            value={info.title || ""}
+            onChange={handleChange}
             required
             />
             <label htmlFor="title">Description</label>
@@ -19,9 +34,11 @@ const Addtutorials = () => {
             id='description'
             name='description'
             className='form-control'
+            value={info.description || ""}
+            onChange={handleChange}
             required
             />
-            <button className='btn btn-danger mt-2'>Submit</button>
+            <button type="submit" className='btn btn-danger mt-2'>Submit</button>
         </form>
     </div>
   )
