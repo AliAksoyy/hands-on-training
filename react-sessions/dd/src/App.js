55fc6ce9efff1fcf628fd6b2839b6d5adf0cbca6
AliAksoyy <alifeyza4017@gmail.com>
2023-01-01 13:14:46 +0300

    add about

--- react-sessions/dd/src/App.js
+++ react-sessions/dd/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import About from './components/About'
+import Footer from './components/Footer'
+import Nav from './components/Nav'
 import Home from './page/Home'
-
+import Instructors from './page/Instructors'
+import NotFound from './page/NotFound'
 const App = () => {
   return (
-    <div className="app"><Home /></div>
+    <div>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/instructors' element={<Instructors />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
 
--- react-sessions/dd/src/page/Home.jsx
+++ react-sessions/dd/src/page/Home.jsx
@@ -1,41 +1,8 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import AddTask from '../components/AddTask'
-import Header from '../components/Header'
-import ListTask from '../components/ListTask'
+import React from 'react'
 
 const Home = () => {
-
-  const [show,setShow]=useState(false)
-  const [info,setInfo]=useState([])
-
-  useEffect(()=> {
-    getData()
-  },[])
-
-const getData = async()=> {
-  const url = "https://63b118c86a74151a1bcae4be.mockapi.io/api/task"
-  const {data} =await axios(url)
-  setInfo(data)
-}
-
-console.log(info)
-
   return (
-    <div style={{width:"55%", border:"4px solid orange",borderRadius:"15px", padding:"1rem", backgroundColor:"#ef1"}}>
-      <Header show={show} setShow={setShow} />
-      {show && (
-        <>
-        <AddTask info ={info} setInfo={setInfo} getData={getData} />
-        <ListTask info ={info} setInfo={setInfo} />
-        </>
-      )}
-       
-       
-      
-    
-    </div>
+    <div>Home</div>
   )
 }
 
