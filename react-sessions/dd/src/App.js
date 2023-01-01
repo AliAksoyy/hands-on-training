5b2b96b9b4e938109c9e1fe1fc3c8221c88879fd
AliAksoyy <alifeyza4017@gmail.com>
2023-01-01 14:03:43 +0300

    add course

--- react-sessions/dd/src/App.js
+++ react-sessions/dd/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import About from './components/About'
-import Footer from './components/Footer'
-import Nav from './components/Nav'
-import Home from './page/Home'
-import Instructors from './page/Instructors'
-import NotFound from './page/NotFound'
-const App = () => {
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import People from "./pages/People";
+import NotFound from "./pages/NotFound";
+
+function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/instructors' element={<Instructors />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  )
+    <>
+      <Nav />
+      <Home />
+      <People />
+      <Contact />
+      <NotFound />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
