f7aeeb0e496abeef7acddd5d0b6b10e2ae7c846a
AliAksoyy <alifeyza4017@gmail.com>
2023-01-01 09:07:19 +0300

    add show bar

--- react-sessions/dd/src/components/Header.jsx
+++ react-sessions/dd/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import img from "../assets/task.png"
-const Header = () => {
+const Header = ({show,setShow}) => {
   return ( <>
     <div className='d-flex justify-content-around align-items-center'>
         <img style={{width:"7rem"}} src={img} alt="s" />
         <h2>TASK TRACKER</h2>
     </div>
     <div className="d-flex justify-content-center">
-        <button className="btn btn-danger mt-2 p-1">Hide Task Bar</button>
+        <button onClick={()=> setShow(!show)} className="btn btn-danger mt-2 p-1">
+            {show ? "HİDE" :"SHOW"}
+        </button>
     </div>
     </>)
 }
--- react-sessions/dd/src/page/Home.jsx
+++ react-sessions/dd/src/page/Home.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AddTask from '../components/AddTask'
 import Header from '../components/Header'
 import ListTask from '../components/ListTask'
 
 const Home = () => {
+
+  const [show,setShow]=useState(false)
   return (
     <div style={{width:"55%", border:"4px solid orange",borderRadius:"15px", padding:"1rem", backgroundColor:"#ef1"}}>
-      <Header />
-      <AddTask />
-      <ListTask />
+      <Header show={show} setShow={setShow} />
+      {show && (
+        <>
+        <AddTask />
+        <ListTask />
+        </>
+      )}
+       
+       
+      
+    
     </div>
   )
 }
