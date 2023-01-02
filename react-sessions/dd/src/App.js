1e6e9f73f08ec21554fd6edabc1335bb31ea6c5a
AliAksoyy <alifeyza4017@gmail.com>
2023-01-02 16:29:05 +0300

    added css

--- react-sessions/dd/src/App.js
+++ react-sessions/dd/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Button } from './components/Button.styled'
 import { Container } from './components/Container'
+import H1 from './components/Header.styled'
 
 const App = () => {
   return (
     <Container>
+      <H1 color={"red"}>Ali aksoy</H1>
       <Button primary >Button</Button>
       <Button >Button</Button>
     </Container>
