d3f5eda59488275ac22cb9e9c992d77f6cda7a8e
AliAksoyy <alifeyza4017@gmail.com>
2023-01-02 16:34:09 +0300

    add img

--- react-sessions/dd/src/App.js
+++ react-sessions/dd/src/App.js
@@ -1,16 +1,9 @@
-import React from 'react'
-import { Button } from './components/Button.styled'
-import { Container } from './components/Container'
-import H1 from './components/Header.styled'
-
 const App = () => {
   return (
-    <Container>
-      <H1 color={"red"}>Ali aksoy</H1>
-      <Button primary >Button</Button>
-      <Button >Button</Button>
-    </Container>
-  )
-}
+    <>
+      <h1>APP</h1>
+    </>
+  );
+};
 
-export default App
+export default App;
--- react-sessions/dd/src/components/Header.styled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const H1 = styled.h1`
-  color: ${({color})=> color || "black"}
-`;
-
-export default H1;
