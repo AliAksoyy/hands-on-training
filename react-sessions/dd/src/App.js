1065f20d628765447df289383eb48c02464138e0
AliAksoyy <alifeyza4017@gmail.com>
2023-01-04 13:09:23 +0300

    new start

--- react-sessions/dd/src/App.js
+++ react-sessions/dd/src/App.js
@@ -1,31 +1,8 @@
 import React from 'react'
-import AppRouter from './routes/AppRouter'
-import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
-import { GlobalStyle } from './styles/GlobalStyles'
-
 
 const App = () => {
-
-const theme = {
-  colors:{
-    body:"#e21",
-    header:"#a31",
-  },
-  responsive:"500px",
-}
-
-
-
   return (
-    <div>
-    <BrowserRouter>
-    <ThemeProvider theme={theme}>
-     <AppRouter />
-     <GlobalStyle />
-    </ThemeProvider> 
-    </BrowserRouter>
-    </div>
+    <div>App</div>
   )
 }
 
